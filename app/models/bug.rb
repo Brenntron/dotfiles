07bf2432e531d9b1a8cb22514fda5aa641077158
aliciakw <alicia.willett@gmail.com>
2015-02-16 09:44:52 -0500

    reorganize seeds

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,6 +2,7 @@ class Bug < ActiveRecord::Base
   has_many :attachments, :dependent => :destroy
   has_many :exploits, :through => :references
   has_many :jobs, :dependent => :destroy
+  has_many :rules
 
   has_and_belongs_to_many :references
 
