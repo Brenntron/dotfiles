2ba95c60507cb784c2c8787cbfc3ee75bf185302
aliciakw <alicia.willett@gmail.com>
2015-03-23 17:10:20 -0400

    save note to bugzilla

--- app/controllers/api/v1/notes.rb
+++ app/controllers/api/v1/notes.rb
@@ -15,10 +15,21 @@ module API
             requires :text, type: String, desc: "The text of the note."
             requires :author, type: String, desc: "Who wrote the note."
             requires :note_type, type: String, desc: "Is it a Research note or a Committer note?"
-            requires :bugzilla_id, type: Integer, desc: "The bug the note pertains to."
+            requires :bugzilla_id, type: Integer, desc: "The id or alias of the bug to append a comment to."
+            optional :is_private, type: Boolean, desc: "If set to true, the comment is private, otherwise it is assumed to be public."
+            optional :is_markdown, type: Boolean, desc: "If set to true, the comment has Markdown structures, otherwise it is a normal text."
+            optional :minor_update, type: Boolean, desc: "If set to true, this is considered a minor update and no mail is sent to users who do not want minor update emails. If current user is not in the minor_update_group, this parameter is simply ignored."
           end
         end
         post "", root: "note" do
+          options = {
+            :id => permitted_params[:note][:bugzilla_id],
+            :comment => permitted_params[:note][:text],
+            :is_private => permitted_params[:note][:is_private],
+            :is_markdown => permitted_params[:note][:is_markdown],
+            :minor_update => permitted_params[:note][:minor_update]
+          }.reject() { |k, v| v.nil? }
+          Bugzilla::Bug.new(bugzilla_session).add_comment(options)
           Note.create(
             :text => permitted_params[:note][:text],
             :author => permitted_params[:note][:author],
@@ -47,9 +58,7 @@ module API
         params do
           requires :id, type: Integer, desc: "The note's id in the database."
           requires :note, type: Hash do
-            #requires :id, type: Integer, desc: "The bug to which the note pertains."
             requires :text, type: String, desc: "The text of the note."
-            #optional :note_type, type: String, desc: "Is it a Research note or a Committer note?"
           end
         end
         put ":id", root: "note" do
