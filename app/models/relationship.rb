ec52748aaaa8b8d8f55eb44e34d899f3037447c1
aliciakw <alicia.willett@gmail.com>
2015-04-02 11:18:18 -0400

    setup team manager relationships

---
+++ app/models/relationship.rb
@@ -0,0 +1,5 @@
+class Relationship < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :team_member,
+             :class_name => 'User'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324170413) do
+ActiveRecord::Schema.define(version: 20150401202645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,13 @@ ActiveRecord::Schema.define(version: 20150324170413) do
   end
 
   add_index "references", ["rule_id"], name: "index_references_on_rule_id", using: :btree
+
+  create_table "relationships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "team_member_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "rules", force: true do |t|
     t.integer  "gid"
