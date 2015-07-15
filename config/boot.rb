0edbb890f060b60ab7618ade5d8225dec8624c8a
NickHerbert <nherbe3@gmail.com>
2015-07-15 11:17:39 -0400

    updating boot to use standalone

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,13 @@
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+begin
+  # use `bundle install --standalone' to get this...
+  require_relative '..vendor/bundle/bundler/setup'
+  puts "USING STANDALONE BUNDLER"
+rescue LoadError
+  # fall back to regular bundler if the developer hasn't bundled standalone
+  # Set up gems listed in the Gemfile.
+  ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+  require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+  puts "USING STANDARD BUNDLER"
+end
 
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
