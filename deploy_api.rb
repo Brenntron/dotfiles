3ff6bd020bd7492781e9a6d668b45ea88e392bc7
NickHerbert <nherbe3@gmail.com>
2015-07-31 10:24:12 -0400

    removed bundler condition in boot.rb moved deploy into both apps

---
+++ deploy_api.rb
@@ -0,0 +1,140 @@
+require 'fileutils'
+require 'digest'
+
+
+def red(str)
+  "\e[31m#{str}\e[0m"
+end
+# Figure out the name of the current local branch
+def self.current_git_branch
+  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
+  puts "Deploying branch #{red branch}"
+  branch
+end
+
+
+def self.build_API
+  puts "delete production folder"
+  production_folder = "production"
+  if File.directory?("../#{production_folder}")
+    puts "production folder exists. deleting it now."
+    FileUtils.rm_rf("../#{production_folder}")
+  end
+
+
+  puts "clone the git repo to the production folder"
+  system "git clone git@github.com:talosweb/talos_api.git -b #{current_git_branch} --single-branch ../production"
+
+
+  puts "build the gems into vendor/bundle"
+  Dir.chdir "../production"
+  system 'bundle install --deployment'
+  system 'bundle package'
+  system 'bundle install --standalone'
+  Dir.chdir ".."
+
+  puts "tar up the contents of the production folder"
+  system 'cd production/ && tar -zcvf ../rulesuitest.tar.gz . && cd ..'
+end
+
+def self.upload_API(rebuild_gems)
+  puts "create a new folder on the server with a new timestamp"
+  timestamp = Time.now.to_i
+  `ssh talosweb@rulesuitest.vrt.sourcefire.com << ENDSSH
+            mkdir rulesuitest/releases/#{timestamp}
+            ENDSSH`
+
+  puts "scp the tarball to talosweb@rulesuitest.vrt.sourcefire.com:rulesuitest/public/app folder"
+  system "scp rulesuitest.tar.gz talosweb@rulesuitest.vrt.sourcefire.com:rulesuitest/releases/#{timestamp}"
+
+  puts "unload the zip file into timestamp folder"
+  `ssh talosweb@rulesuitest.vrt.sourcefire.com << ENDSSH
+            cd rulesuitest/releases/#{timestamp}/
+            tar -zxvf rulesuitest.tar.gz
+            rm rulesuitest.tar.gz
+            ENDSSH`
+
+  puts "copy the app config and the database yaml files to the timestamp folder"
+  `ssh talosweb@rulesuitest.vrt.sourcefire.com << ENDSSH
+            rm /usr/local/www/rulesuitest/releases/#{timestamp}/config/database.yml
+            rm /usr/local/www/rulesuitest/releases/#{timestamp}/config/app_config.yml
+            rm /usr/local/www/rulesuitest/releases/#{timestamp}/config/secrets.yml
+            ln -s /usr/local/www/rulesuitest/releases/shared/secrets.yml /usr/local/www/rulesuitest/releases/#{timestamp}/config/secrets.yml
+            ln -s /usr/local/www/rulesuitest/releases/shared/database.yml /usr/local/www/rulesuitest/releases/#{timestamp}/config/database.yml
+            ln -s /usr/local/www/rulesuitest/releases/shared/app_config.yml /usr/local/www/rulesuitest/releases/#{timestamp}/config/app_config.yml
+            ENDSSH`
+
+  puts "simlink the timestamped folder to the app directory"
+  `ssh talosweb@rulesuitest.vrt.sourcefire.com << ENDSSH
+            rm /usr/local/www/rulesuitest/public/app
+            ln -s /usr/local/www/rulesuitest/releases/#{timestamp} /usr/local/www/rulesuitest/public/app
+            ENDSSH`
+
+
+  puts "build the gems locally if folder exists"
+  # if vendor folder exists copy that over to this vendor folder other wise build the gems
+  # and save a copy of the vendor folder for next time.
+  directory_exists = `ssh talosweb@rulesuitest.vrt.sourcefire.com "test -d /usr/local/www/rulesuitest/releases/shared/vendor && echo 1 || echo 0"`
+  if(directory_exists.to_i == 1 && rebuild_gems == false)
+    `ssh talosweb@rulesuitest.vrt.sourcefire.com << ENDSSH
+            echo "copying vendor to vendor"
+            rm -rf /usr/local/www/rulesuitest/releases/#{timestamp}/vendor
+            cp -r /usr/local/www/rulesuitest/releases/shared/vendor /usr/local/www/rulesuitest/releases/#{timestamp}/vendor
+            ENDSSH`
+  else
+    `ssh talosweb@rulesuitest.vrt.sourcefire.com << ENDSSH
+            echo "bundle installing gems"
+            cd rulesuitest/releases/#{timestamp}/
+            bundle install --deployment
+            cp -r /usr/local/www/rulesuitest/releases/#{timestamp}/vendor /usr/local/www/rulesuitest/releases/shared/vendor
+            ENDSSH`
+  end
+
+  puts "add tmp/restart.txt"
+  `ssh talosweb@rulesuitest.vrt.sourcefire.com << ENDSSH
+            cd rulesuitest/releases/#{timestamp}/
+            mkdir tmp
+            touch tmp/restart.txt
+            ENDSSH`
+end
+process_api = true
+send_upload = true
+rebuild_gems = false
+
+ARGV.each do|a|
+  case a
+    when "--no-api"
+      process_api = false
+    when "--no-upload"
+      send_upload = false
+    when "--rebuild-gems"
+      rebuild_gems = true
+    when "-h" || "-help"
+      puts "This script deploys all the content in the API directory and the UI directory up to the server"
+      puts "============"
+      puts "Valid flags are"
+      puts "--no-api           use this flag to prevent the API from being built and sent to the server"
+      puts "--no-upload        apps will be built but they will be prevented from being sent to the server"
+      puts "--rebuild-gems     gems are stored in the shared folder on the server. Use this flag to rebuild them."
+      process_api = false
+      send_upload = false
+      break
+    else
+      puts "One of your flags '#{a}' is not valid. Try again. use -h or --help"
+      process_api = false
+      send_upload = false
+      break
+  end
+end
+
+if process_api
+  build_API()
+  if send_upload
+    upload_API(rebuild_gems)
+  end
+end
+
+
+
+
+
