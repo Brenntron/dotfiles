4594dc217222c8ed75e65e420ea8312820890678
aliciakw <alicia.willett@gmail.com>
2015-10-15 12:01:02 -0400

    apply to all models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
 class Event < ActiveRecord::Base
 
+  after_create {|event| event.record 'create' }
+  after_update {|event| event.record 'update' }
+  after_destroy {|event| event.record 'destroy' }
+
+  def record action
+    record = { resource: 'event',
+               action: action,
+               id: self.id,
+               obj: self }
+    PublishWebsocket.push_changes(record)
+  end
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,16 @@ class Job < ActiveRecord::Base
   has_many :rules
   has_many :attachments
 
+  after_create {|job| job.record 'create' }
+  after_update {|job| job.record 'update' }
+  after_destroy {|job| job.record 'destroy' }
+
+  def record action
+    record = { resource: 'job',
+               action: action,
+               id: self.id,
+               obj: self }
+    PublishWebsocket.push_changes(record)
+  end
+
 end
