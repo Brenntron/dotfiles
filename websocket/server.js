ca349f5db79a433e5c4d62de302d6e212d5cdbd4
aliciakw <alicia.willett@gmail.com>
2015-10-23 10:29:17 -0400

    cleanup node servers

--- websocket/server.js
+++ websocket/server.js
@@ -2,7 +2,6 @@
 // This server can connect to a websocket to publish the changes from activeMQ
 
 var WebSocketServer = require('ws').Server;
-var ws = new WebSocketServer({port: 7001});
 var Stomp = require('stompjs');
 var client = Stomp.overTCP('localhost', 61613);
 var app = require('express')();
@@ -14,13 +13,13 @@ var headers = {
 
 var socket = require('socket.io-client')('https://localhost:7000');
 socket.on('connect', function(){
-  console.log('amq server connected to websocket');
+  console.log('Connected to the websocket.');
 });
 function publishToWebsocket(msg){
   socket.emit('amq', msg);
 }
 client.connect(headers, function() {
-  console.log("Connected to ActiveMQ with Stomp");
+  console.log("Connected to ActiveMQ with Stomp.");
   client.subscribe("/queue/RulesUI.Snort.Run.Local.Test.Work", function(message) {
     var data = JSON.parse(message.body);
     publishToWebsocket(JSON.parse(data["record"]));
