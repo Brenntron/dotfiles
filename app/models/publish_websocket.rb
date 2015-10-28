4973dbd523359121abb3d4bbba4d25fb2e272c9d
NickHerbert <nherbe3@gmail.com>
2015-10-28 15:33:39 -0400

    updating where messages go

--- app/models/publish_websocket.rb
+++ app/models/publish_websocket.rb
@@ -3,10 +3,10 @@ require 'json'
 include ActiveMessaging::MessageSender
 
 class PublishWebsocket
-  publishes_to :snort_local_rules_test_work
+  publishes_to :snort_local_rules_test_messages
 
   def self.push_changes(record)
-    publish :snort_local_rules_test_work, {
+    publish :snort_local_rules_test_messages, {
         :record => record.to_json
     }.to_json
   end
