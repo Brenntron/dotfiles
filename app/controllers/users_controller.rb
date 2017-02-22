da862c0094415a3fd2768a868200ae17302b87f3
Marlin W. Pierce <marlpier@cisco.com>
2017-02-22 14:10:58 -0500

    move authenticated_access before_action into save action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
 
   before_action :require_login
-  before_action :authenticate_access, only: [:show]
 
   def index
     @users = current_user.team_members
   end
 
   def show
-    @users = current_user.team_members
-    @user = User.find(params[:id])
+    if !current_user.authorized_user_list.include?(params[:id].to_i)
+      flash[:error] = 'You are not authorized to view that user.'
+      redirect_to users_path
+    else
+      @users = current_user.team_members
+      @user = User.find(params[:id])
+    end
   end
 
   def results
@@ -86,13 +90,6 @@ class UsersController < ApplicationController
 
   def require_login
     redirect_to root_url if !current_user
-  end
-
-  def authenticate_access
-    if !current_user.authorized_user_list.include?(params[:id].to_i)
-      flash[:error] = 'You are not authorized to view that user.'
-      redirect_to users_path
-    end
   end
 
   def user_params
