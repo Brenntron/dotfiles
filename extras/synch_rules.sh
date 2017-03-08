2f414642199259f7027da97f740e6a2e9e87743c
Marlin W. Pierce <marlpier@cisco.com>
2017-03-08 11:45:46 -0500

    display usage when synch_rules.sh is called with no args.

--- extras/synch_rules.sh
+++ extras/synch_rules.sh
@@ -3,6 +3,12 @@
 # parses rule files and runs rails script to load into synched_rules table.
 
 
+if [ $# -eq 0 ]; then
+    echo "USAGE: synch_rules.sh <filenames>"
+    exit 1
+fi
+
+
 grep -Hn "sid:\s*\d\d*\s*;" $* | bundle exec rails runner extras/synch_rules.rb
 
 
