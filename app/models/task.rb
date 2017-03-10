b4983a2339ed217c65644fdaaf9a8994a972b77c
Marlin W. Pierce <marlpier@cisco.com>
2017-03-10 08:34:23 -0500

    Closes #118 record stats from testing rules on rule records.

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,4 +15,30 @@ class Task < ApplicationRecord
                obj: self }
     PublishWebsocket.push_changes(record)
   end
+
+  # Parsed output of results from testing rules
+  def stats
+    result.split("\n").inject({}) do |stats, line|
+      line.lstrip!
+      num, sid, gid, rev, checks, matches, alerts, microsecs, ave_check, ave_match, ave_nonmatch, disabled =
+          line.split(/\s+/)
+      if num =~ /^\d+$/
+        stats[sid.to_i] = { average_check: ave_check, average_match: ave_match, average_nonmatch: ave_nonmatch }
+      end
+
+      stats
+    end
+  end
+
+  # Set stats fields on rule records
+  def update_rule_stats
+    stats.each_pair do |sid, attrs|
+      rule = Rule.by_sid(sid).first
+      if rule && rule.sid
+        rule.update(attrs)
+      end
+    end
+
+    true
+  end
 end
