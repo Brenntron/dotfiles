d7c11411e38405687467525b6c78078c2d8dfe63
NickHerbert <nherbe3@gmail.com>
2017-03-13 15:25:12 -0400

    fixing attachments to rules

--- app/models/test_rule.rb
+++ app/models/test_rule.rb
@@ -4,8 +4,8 @@ class TestRule
   publishes_to :snort_local_rules_test_work
 
   def self.send_work_msg(content, options, request)
-    # be sure to collect all the attachments too but only the ones that are not obsolete
-    all_attachments = options[:bug].attachments.map { |b| b.is_obsolete ? next : b.id }.reject { |v| v.nil? }
+    # be sure to collect all the attachments too but only the ones that are pcaps
+    all_attachments = options[:bug].attachments.map { |b| /^\S+.pcap$/.match(b.file_name) ? b.id : next}.reject { |v| v.nil? }
     # TODO: collect rule content to insert in the local_rules. Dont use numbers.
     rules_content = []
     content.rules.each do |rule|
