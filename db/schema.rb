a7b965459fc2f1ac7746219fd6f6023d93cdeb15
Marlin W. Pierce <marlpier@cisco.com>
2017-04-10 15:10:03 -0400

    Rename open_bug to open_bugs and fix tests.

--- db/schema.rb
+++ db/schema.rb
@@ -268,7 +268,7 @@ ActiveRecord::Schema.define(version: 20170407202629) do
     t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
-  create_table "test_reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "test_reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "task_id",                     null: false
