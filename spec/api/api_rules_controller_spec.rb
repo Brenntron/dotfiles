31c0aa5f3554bf431c812c755d18da4bf1dc973b
Marlin W. Pierce <marlpier@cisco.com>
2017-05-30 11:23:41 -0400

    Call a web service.

--- spec/api/api_rules_controller_spec.rb
+++ spec/api/api_rules_controller_spec.rb
@@ -2,6 +2,8 @@ describe API::V1::Rules do
   describe "GET gids/:gid/sids/:sid", type: :api do
     it "gets a rule" do
       get "/api/v1/rules/gids/1/sids/19500"
+      puts ">>> response.body = #{response.body.inspect}"
+      expect(response).to be_success
     end
   end
 end
