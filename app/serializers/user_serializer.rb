5f8639eb6667cf18efc92951bed7c8eeb0cff20f
Marlin W. Pierce <marlpier@cisco.com>
2017-06-16 09:57:29 -0400

    Removed unused fields which are not anywhere, even the database.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :kerberos_login, :cvs_username, :cec_username, :display_name, :committer, :email, :team_member_ids, :manager_ids
+  attributes :id, :kerberos_login, :cvs_username, :cec_username, :display_name, :committer, :email
   has_many :bugs, embed: :ids, embed_in_root: true
 end
