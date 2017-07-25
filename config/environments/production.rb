fe48ef2cc161b38ac764d5206f15ea05a8895723
NickHerbert <nherbe3@gmail.com>
2017-07-25 11:24:59 -0400

    changed loging to syslogger in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,12 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  require 'syslog/logger'
+  config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'analyst-console')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
-
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
