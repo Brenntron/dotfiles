fe5483a47fcac1b67f8e98ad91527d6defe5b332
Marlin W. Pierce <marlpier@cisco.com>
2017-07-25 13:53:19 -0400

    Undoing unnecessary changes.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,24 +68,18 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-  
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+
+  # Disable automatic flushing of the log to improve performance.
+  # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'analyst-console')
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
