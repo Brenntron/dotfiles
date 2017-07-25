f93b6006f422b6df8f4b7cda907a15134b4e8d8d
Marlin W. Pierce <marlpier@cisco.com>
2017-07-25 16:31:05 -0400

    Remove extra comment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -71,9 +71,6 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-  
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
