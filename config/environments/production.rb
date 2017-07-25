6f8c5e30aa78a61bde0280e287a49714677cb908
NickHerbert <nherbe3@gmail.com>
2017-07-25 11:37:18 -0400

    cleaning up code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+require 'syslog/logger'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -77,12 +78,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
-  require 'syslog/logger'
   config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'analyst-console')
 
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
