d034a5a908089fb8db6bbe88dfa6dcc144ea7681
Marlin W. Pierce <marlpier@cisco.com>
2017-08-02 11:45:15 -0400

    Set staging to use bugzilla test.

--- config/initializers/bugzilla.rb
+++ config/initializers/bugzilla.rb
@@ -3,7 +3,8 @@ case
   when Rails.env.production?
     Rails.configuration.bugzilla_host = 'bugzilla.vrt.sourcefire.com'
   when Rails.env.staging?
-    Rails.configuration.bugzilla_host = 'fmd-bugzil-01stg.vrt.sourcefire.com'
+    # Rails.configuration.bugzilla_host = 'fmd-bugzil-01stg.vrt.sourcefire.com'
+    Rails.configuration.bugzilla_host = 'bugzillaTest02.vrt.sourcefire.com'
   else
     Rails.configuration.bugzilla_host = 'bugzillaTest02.vrt.sourcefire.com'
 end
