f831951ff2eba16b6d7f8729cf2e4b2376a74c02
NickHerbert <nherbe3@gmail.com>
2017-08-02 12:29:15 -0400

    Noticed a method wasnt properly formed added begin keyword to fix

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,38 +21,39 @@ class NotesController < ApplicationController
   end
 
   def publish_to_bugzilla
-    options = {
-        :id => params[:note][:bugzilla_id],
-        :comment => params[:note][:comment],
-        :note_type => params[:note][:note_type],
-        :author => current_user.email,
-        :is_private => params[:note][:is_private],
-        :is_markdown => params[:note][:is_markdown],
-        :minor_update => params[:note][:minor_update]
-    }.reject() { |k, v| v.nil? }
-    new_note = Bugzilla::Bug.new(bugzilla_session).add_comment(options)
-    if params[:note][:id].blank?
-      note = Note.create(id: new_note['id'],
-                         comment: options[:comment],
-                         author: options[:author],
-                         note_type: options[:note_type])
-    else
-      note = Note.where("id=?",params[:note][:id]).first
+    begin
+      options = {
+          :id => params[:note][:bugzilla_id],
+          :comment => params[:note][:comment],
+          :note_type => params[:note][:note_type],
+          :author => current_user.email,
+          :is_private => params[:note][:is_private],
+          :is_markdown => params[:note][:is_markdown],
+          :minor_update => params[:note][:minor_update]
+      }.reject() { |k, v| v.nil? }
+      new_note = Bugzilla::Bug.new(bugzilla_session).add_comment(options)
+      if params[:note][:id].blank?
+        note = Note.create(id: new_note['id'],
+                           comment: options[:comment],
+                           author: options[:author],
+                           note_type: options[:note_type])
+      else
+        note = Note.where("id=?", params[:note][:id]).first
+      end
+      bug = Bug.find params[:note][:bugzilla_id]
+      bug.notes << note
+      if note.update(:id => new_note['id'], :comment => params[:note][:comment], :notes_bugzilla_id => new_note['id'])
+        render json: {bug: bug.as_json, note: note.as_json}
+      else
+        render json: "Published to bugzilla but not updated in local db", status: 422
+      end
+    rescue => e
+      render json: "Error. Could not publish to bugzilla; #{e}", status: 422
     end
-    bug = Bug.find params[:note][:bugzilla_id]
-    bug.notes << note
-    if note.update(:id => new_note['id'],:comment => params[:note][:comment], :notes_bugzilla_id => new_note['id'])
-      render json: {bug: bug.as_json, note: note.as_json}
-    else
-      render json: "Published to bugzilla but not updated in local db", status: 422
-    end
-  rescue => e
-    render json: "Error. Could not publish to bugzilla; #{e}" , status: 422
   end
-end
 
 
-private
+  private
 
 def note_params
   params.require(:note).permit(:comment, :author, :flow, :note_type, :bug_id, :notes_bugzilla_id, :id)
