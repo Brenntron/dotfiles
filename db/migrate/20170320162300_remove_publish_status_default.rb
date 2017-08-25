0a63728ec028605a4e46dd39caed751c08d30322
Marlin W. Pierce <marlpier@cisco.com>
2017-08-25 16:52:06 -0400

    More rules table cleanup.

--- db/migrate/20170320162300_remove_publish_status_default.rb
+++
@@ -1,6 +0,0 @@
-class RemovePublishStatusDefault < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :rules, :publish_status
-    add_column :rules, :publish_status, :string, null: false
-  end
-end
