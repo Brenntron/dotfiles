bc02319e82b16e9e1a6fa950b1acd702fac1b5bf
Marlin W. Pierce <marlpier@cisco.com>
2017-08-25 16:43:19 -0400

    Move rules table creation to 2017 schema.

--- db/migrate/20141114150949_create_references_and_rules.rb
+++ db/migrate/20141114150949_create_references_and_rules.rb
@@ -8,36 +8,6 @@ class CreateReferencesAndRules < ActiveRecord::Migration[4.2]
     # add_reference :references, :rule, index: true
     # add_reference :references, :bug, index: true
 
-    create_table :rules do |t|
-      t.text     "rule_content"
-      t.text     "rule_parsed"
-      t.text     "rule_warnings"
-      t.text     "rule_failures"
-      t.text     "cvs_rule_content"
-      t.text     "cvs_rule_parsed"
-      t.text     "connection"
-      t.string   "message"
-      t.string   "flow"
-      t.text     "detection"
-      t.string   "metadata"
-      t.string   "class_type"
-      t.integer  "gid"
-      t.integer  "sid"
-      t.integer  "rev"
-      t.string   "state"
-      t.string   "publish_status", null: false, default: "SYNCHED"
-      t.float    "average_check"
-      t.float    "average_match"
-      t.float    "average_nonmatch"
-      t.boolean  "tested",           default: false
-      t.boolean  "committed",        default: false
-      t.timestamps
-    end
-
-    add_reference :rules, :task, index: true
-    add_reference :rules, :rule_category, index: true
-    add_index :rules, [:gid, :sid], :name => "index_rules_gid_and_sid", :unique => true
-
     create_table :references_rules, id: false do |t|
       t.belongs_to :reference, index: true
       t.belongs_to :rule, index: true
--- db/migrate/20170801120000_schema2017.rb
+++ db/migrate/20170801120000_schema2017.rb
@@ -1,4 +1,37 @@
 class Schema2017 < ActiveRecord::Migration[5.1]
   def change
+    create_table "rules", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.integer "gid"
+      t.integer "sid"
+      t.integer "rev"
+      t.string "filename"
+      t.integer "linenumber"
+      t.text "rule_content"
+      t.text "rule_parsed"
+      t.text "rule_warnings"
+      t.text "rule_failures"
+      t.text "cvs_rule_content"
+      t.text "cvs_rule_parsed"
+      t.text "connection"
+      t.string "message"
+      t.string "flow"
+      t.text "detection"
+      t.string "metadata"
+      t.string "class_type"
+      t.bigint "task_id"
+      t.bigint "rule_category_id"
+      t.string "state"
+      t.string "edit_status", null: false
+      t.boolean "parsed", default: true, null: false
+      t.boolean "on", default: true, null: false
+      t.string "publish_status", null: false
+      t.boolean "tested", default: false
+      t.boolean "committed", default: false
+      t.index ["gid", "sid"], name: "index_rules_gid_and_sid", unique: true
+      t.index ["rule_category_id"], name: "index_rules_on_rule_category_id"
+      t.index ["task_id"], name: "index_rules_on_task_id"
+    end
   end
 end
