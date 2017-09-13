68166e378df41ece078b234f7cf9b737073fc85e
NickHerbert <nherbe3@gmail.com>
2017-09-12 22:04:31 -0400

    removed space from helper function

--- app/helpers/bugs_helper.rb
+++ app/helpers/bugs_helper.rb
@@ -38,12 +38,11 @@ def set_bug_color(bug)
   def bug_filter_helper
     case session[:query]
       when 'advance-search'
-        ' Search'
+        'Search'
       when ''
-        ' Bugs'
+        'Bugs'
       else
-        ' ' + session[:query].titleize
-
+        session[:query].titleize
     end
   end
 
