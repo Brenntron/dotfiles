137a988dc9a7a54686d4a08c662760f3cdeb524e
Marlin W. Pierce <marlpier@cisco.com>
2017-10-09 10:20:04 -0400

    Added spec to load files missing from test coverage.

---
+++ spec/unreffed_spec.rb
@@ -0,0 +1,24 @@
+# The classes below were not being loaded,
+# so our test coverage was invalid
+#
+# The code below loads those classes to correct for this.
+
+describe Admin::HomeController
+describe Admin::MigrationsController
+
+describe EventsController
+describe ReferencesController
+describe RolesController
+
+describe ApiTest::EngineTypesController
+describe ApiTest::EnginesController
+describe ApiTest::JobsController
+describe ApiTest::PcapsController
+describe ApiTest::RuleConfigurationsController
+describe ApiTest::SnortConfigurationsController
+
+describe Repo::RuleCommitter
+describe Repo::RuleDocCommitter
+describe RuleEvent::RuleCommitEvent
+describe RuleSyntax::Metadata
+
