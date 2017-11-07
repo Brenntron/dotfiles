a5b97f3740ae26f4ce5510871f39d273f28cb07a
NickHerbert <nherbe3@gmail.com>
2017-11-07 12:06:03 -0500

    commenting out all the Delayed Job stuff for importing bugs

--- app/assets/javascripts/bugs.js.coffee
+++ app/assets/javascripts/bugs.js.coffee
@@ -585,9 +585,9 @@ namespace 'AC.Bugs', (exports) ->
 
   exports.monitorJobQueue = () ->
     AC.Bugs.rebuildAllTabs()
-    setInterval ->
-      AC.Bugs.rebuildAllTabs()
-    , 40000
+#    setInterval ->
+#      AC.Bugs.rebuildAllTabs()
+#    , 40000
 
 
   exports.rebuildAllTabs = () ->
--- app/controllers/admin/scheduled_tasks_controller.rb
+++ app/controllers/admin/scheduled_tasks_controller.rb
@@ -12,11 +12,11 @@ class Admin::ScheduledTasksController < Admin::HomeController
     task_type = params[:task_type]
     start_time = params.require(:run_at).permit(:year,:month,:day,:hour,:minute).to_h
     run_at = Time.new(start_time[:year],start_time[:month],start_time[:day],start_time[:hour],start_time[:minute])
-    if Task.schedule_task(task_type, run_at, re_run, current_user, bugzilla_session)
-      redirect_to admin_scheduled_tasks_path, :notice => "Job has been queued."
-    else
+    # if Task.schedule_task(task_type, run_at, re_run, current_user, bugzilla_session)
+    #   redirect_to admin_scheduled_tasks_path, :notice => "Job has been queued."
+    # else
       redirect_to admin_scheduled_tasks_path, :notice => "Job create encountered an error"
-    end
+    # end
 
   end
 
