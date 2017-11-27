14f8ec4d8be78feb04da457d562aeaad29a0ffc2
Marlin W. Pierce <marlpier@cisco.com>
2017-11-27 15:44:02 -0500

    Use value from sid in rule content.

--- test/factories/rules.rb
+++ test/factories/rules.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     gid                 1
     sequence(:sid)      {|n| 19500 + n }
     rev                 3
-    rule_content "alert tcp $EXTERNAL_NET $FILE_DATA_PORTS -> $HOME_NET any (msg:\"BROWSER-PLUGINS Microsoft Internet Explorer MSXML .definition ActiveX clsid access attempt\"; flow:to_client,established; file_data; content:\"Msxml2.FreeThreadedDOMDocument.6.0\"; fast_pattern:only; content:\".definition(\"; nocase; pcre:\"/(var|set)\\s+\\w+\\s*=\\s*(new\\s+ActiveXObject|CreateObject)\\s*\\((?P<q1>(\\x22|\\x27|))Msxml2\\.FreeThreadedDOMDocument\\.6\\.0(?P=q1)\\)/smi\"; metadata:policy balanced-ips drop, policy security-ips drop, service ftp-data, service http, service imap, service pop3; reference:cve,2012-1889; reference:url,technet.microsoft.com/en-us/security/bulletin/ms12-043; classtype:attempted-user; sid:23304; rev:4;)"
+    rule_content { "alert tcp $EXTERNAL_NET $FILE_DATA_PORTS -> $HOME_NET any (msg:\"BROWSER-PLUGINS Microsoft Internet Explorer MSXML .definition ActiveX clsid access attempt\"; flow:to_client,established; file_data; content:\"Msxml2.FreeThreadedDOMDocument.6.0\"; fast_pattern:only; content:\".definition(\"; nocase; pcre:\"/(var|set)\\s+\\w+\\s*=\\s*(new\\s+ActiveXObject|CreateObject)\\s*\\((?P<q1>(\\x22|\\x27|))Msxml2\\.FreeThreadedDOMDocument\\.6\\.0(?P=q1)\\)/smi\"; metadata:policy balanced-ips drop, policy security-ips drop, service ftp-data, service http, service imap, service pop3; reference:cve,2012-1889; reference:url,technet.microsoft.com/en-us/security/bulletin/ms12-043; classtype:attempted-user; sid:#{sid}; rev:#{rev};)" }
     rule_parsed "Connection: alert tcp $EXTERNAL_NET $FILE_DATA_PORTS -> $HOME_NET any\nMessage   : BROWSER-PLUGINS Microsoft Internet Explorer MSXML .definition ActiveX clsid access attempt\nFlow      : to_client,established\nDetection :\n\tfile_data;\n\tcontent:\"Msxml2.FreeThreadedDOMDocument.6.0\"; fast_pattern:only;\n\tcontent:\".definition(\"; nocase;\n\tpcre:\"/(var|set)\\s+\\w+\\s*=\\s*(new\\s+ActiveXObject|CreateObject)\\s*\\((?P<q1>(\\x22|\\x27|))Msxml2\\.FreeThreadedDOMDocument\\.6\\.0(?P=q1)\\)/smi\";\nMetadata  :\n\tPolicy: balanced-ips drop, security-ips drop\n\tService: ftp-data, imap, pop3, http\nReferences:\n\tCVE:     2012-1889\n\tBUGTRAQ: <MISSING>\n\tURL:     technet.microsoft.com/en-us/security/bulletin/ms12-043\nClasstype : attempted-user\nSid       : 23304\nRev       : 4"
     connection "alert tcp $EXTERNAL_NET $FILE_DATA_PORTS -> $HOME_NET any"
     message "BROWSER-PLUGINS Microsoft Internet Explorer MSXML .definition ActiveX clsid access attempt"
@@ -35,6 +35,8 @@ FactoryGirl.define do
       publish_status      Rule::PUBLISH_STATUS_CURRENT_EDIT
       edit_status         Rule::EDIT_STATUS_EDIT
       doc_status          Rule::DOC_STATUS_UPDATED
+      cvs_rule_content    { "alert tcp $EXTERNAL_NET $FILE_DATA_PORTS -> $HOME_NET any (msg:\"BROWSER-PLUGINS ActiveX clsid access attempt\"; sid:#{sid}; rev:#{rev};)" }
+      cvs_rule_parsed     "Connection: alert tcp $EXTERNAL_NET $FILE_DATA_PORTS -> $HOME_NET any"
 
       before(:create) do |rule, evaluator|
         rule_grep_line = Rule.grep_line_from_file(rule.sid, rule.gid)
