6b3aca398ce2b15862965a06884715f8dd321ee0
Marlin W. Pierce <marlpier@cisco.com>
2017-12-08 15:08:43 -0500

    Migration clean up.

--- db/migrate/20161021150451_create_rule_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateRuleCategories < ActiveRecord::Migration[4.2]
-  def change
-    create_table :rule_categories do |t|
-      t.string :category
-
-      t.timestamps
-    end
-  end
-end
