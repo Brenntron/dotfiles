7243a8d421a26a5ea17d02338497cee38111d9f0
Marlin W. Pierce <marlpier@cisco.com>
2018-01-18 14:51:11 -0500

    Update SQL for DBA.

--- db/sql/20180109194224_create_false_positive_file_refs.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE `false_positive_file_refs` (`id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY, `created_at` datetime NOT NULL, `updated_at` datetime NOT NULL, `false_positive_id` int, `file_ref_type` varchar(255), `file_ref_id` int) ENGINE=InnoDB;
-INSERT INTO `schema_migrations` (`version`) VALUES ('20180109194224');
