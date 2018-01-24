aab9cfae2e56fbc738f9c64f6c25f5c83643d20c
Chris LaClair <claclair@cisco.com>
2018-01-24 11:38:11 -0500

    Closes #1406: added hyperlink to attachments listed on rules tab

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -9,11 +9,11 @@ class Attachment < ApplicationRecord
   scope :pcap, -> { where("attachments.file_name like '%.pcap'").where.not(is_obsolete: true) }
   scope :left_pcap_test, ->(rule) {
     joins("LEFT OUTER JOIN alerts ON alerts.attachment_id = attachments.id and alerts.test_group = '#{Alert::TEST_GROUP_PCAP}' and alerts.rule_id = #{rule.id}")
-        .select(:file_name, 'alerts.rule_id', 'id')
+        .select(:file_name, 'alerts.rule_id', 'id', 'direct_upload_url')
   }
   scope :left_local_test, ->(rule) {
     joins("LEFT OUTER JOIN alerts ON alerts.attachment_id = attachments.id and alerts.test_group = '#{Alert::TEST_GROUP_LOCAL}' and alerts.rule_id = #{rule.id}")
-        .select(:file_name, 'alerts.rule_id', 'id')
+        .select(:file_name, 'alerts.rule_id', 'id', 'direct_upload_url')
   }
 
   after_create { |attachment| attachment.record 'create' if Rails.configuration.websockets_enabled == 'true' }
