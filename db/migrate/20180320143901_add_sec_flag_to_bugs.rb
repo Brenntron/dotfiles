29446a10fab17cde75c66bdad826d684817a9dfe
Chris LaClair <claclair@cisco.com>
2018-03-22 10:38:10 -0400

    Closes #1569: added sql stuff

--- db/migrate/20180320143901_add_sec_flag_to_bugs.rb
+++ db/migrate/20180320143901_add_sec_flag_to_bugs.rb
@@ -1,5 +1,5 @@
 class AddSecFlagToBugs < ActiveRecord::Migration[5.1]
   def change
-    add_column :bugs, :snort_secure, :boolean
+    add_column :bugs, :snort_secure, :boolean, default: false
   end
 end
