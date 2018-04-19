afb5135ce81878192b5a86c04d7c67691b1e7650
Marlin W. Pierce <marlpier@cisco.com>
2018-04-19 15:49:52 -0400

    Replace crud actions with :manage, except for Bug.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,15 +7,11 @@ class Ability
 
     if current_user.has_role?('admin')
       can :manage, [Admin, User]
-      can [:read, :acknowledge_bug], Bug
+      can [:read, :acknowledge_bug, :list_research, :list_escalations], Bug
       can :manage, RuleDoc
-      # can :list_escalations, Bug
-      can :list_research, Bug
-      can :list_escalations, Bug
     end
     if current_user.has_role?('manager')
-      # can :list_escalations, Bug
-      can :list_research, Bug
+      can [:list_research, :list_escalations], Bug
       can :manage, User do |user|
         user.ancestors.include?(current_user)
       end
@@ -24,26 +20,22 @@ class Ability
       can :list_escalations, Bug
     end
     if current_user.has_role?('committer')
-      can :list_research, Bug
-      can [:update, :destroy, :create], [Bug, Rule, Attachment, Note, Exploit, Reference, RuleDoc]
+      can [:read, :update, :destroy, :create, :list_research, :acknowledge_bug, :toggle_liberty], Bug
+      can :manage, [Rule, Attachment, Note, Exploit, Reference, RuleDoc]
       can :publish, Rule
       can :publish_to_bugzilla, Note
       can :update_preferences, User, id: current_user.id
-      can :toggle_liberty, Bug
-      can :acknowledge_bug, Bug
     end
     if current_user.has_role?('analyst')
-      can :list_research, Bug
-      can [:update, :destroy, :create], [Bug, Rule, Attachment, Note, Exploit, Reference, RuleDoc]
+      can [:read, :update, :destroy, :create, :list_research, :acknowledge_bug], Bug
+      can :manage, [Rule, Attachment, Note, Exploit, Reference, RuleDoc]
       can :publish_to_bugzilla, Note
       can :update_preferences, User, id: current_user.id
       can :toggle_liberty, Bug do |bug|
         bug.liberty_clear?
       end
-      can :acknowledge_bug, Bug
     end
     if current_user.has_role?('build coordinator')
-      can :list_research, Bug
       cannot [:update, :destroy, :create], [Bug, Rule, Attachment, Note, Exploit, Reference]
       can :update_preferences, User, id: current_user.id
     end
