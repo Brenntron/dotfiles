41b76e88c1a56990ce457439fd13bfceac6eca09
Marlin W. Pierce <marlpier@cisco.com>
2018-05-28 06:38:41 -0400

    Update rails controllers with access control model.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,5 +1,4 @@
 class RolesController < ApplicationController
-  before_action :set_role, only: [:show, :edit, :update, :destroy]
 
   # GET /roles
   # GET /roles.json
@@ -14,6 +13,7 @@ class RolesController < ApplicationController
 
   # GET /roles/1/edit
   def edit
+    @role = Role.find(params[:id])
   end
 
   # POST /roles
@@ -32,6 +32,7 @@ class RolesController < ApplicationController
   # PATCH/PUT /roles/1
   # PATCH/PUT /roles/1.json
   def update
+    @role = Role.find(params[:id])
     respond_to do |format|
       if @role.update(role_params)
         format.html { redirect_to roles_path, notice: 'role was successfully updated.' }
@@ -44,6 +45,7 @@ class RolesController < ApplicationController
   # DELETE /roles/1
   # DELETE /roles/1.json
   def destroy
+    @role = Role.find(params[:id])
     @role.destroy
     respond_to do |format|
       format.html { redirect_to roles_url, notice: 'role was successfully destroyed.' }
@@ -52,11 +54,6 @@ class RolesController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_role
-    @role = Role.find(params[:id])
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def role_params
     params.require(:role).permit(:role)
