96686497bc1211a56f0653135e1ee06ea35e5630
Marlin W. Pierce <marlpier@cisco.com>
2018-06-04 12:51:40 -0400

    Rails convention.

--- app/models/wbrs/rule.rb
+++ app/models/wbrs/rule.rb
@@ -1,12 +1,24 @@
 class Wbrs::Rule < Wbrs::Base
-  attr_accessor :category, :desc_long, :descr, :domain, :is_active, :mnem, :path, :path_hashed, :port, :prefix_id,
+  attr_accessor :category_id, :desc_long, :descr, :domain, :is_active, :mnem, :path, :path_hashed, :port, :prefix_id,
                 :protocol, :subdomain, :truncated
+
+  def active?
+    is_active
+  end
+
+  def truncated?
+    truncated
+  end
+
+  def self.new_from_datum(datum)
+    datum['category_id'] = datum.delete('category')
+    new(datum)
+  end
 
   def self.get_where(categories: nil)
     response = post_request(path: '/v1/cat/rules/get', body: {"categories": categories })
-    Rails.logger.debug(">>> Wbrs::Rule.get #{response.inspect}")
 
     response_body = JSON.parse(response.body)
-    response_body['data'].map {|datum| new(datum)}
+    response_body['data'].map {|datum| new_from_datum(datum)}
   end
 end
