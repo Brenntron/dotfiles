6db96bc36e672512c25d597802821c85bd643e6d
Marlin W. Pierce <marlpier@cisco.com>
2018-06-06 11:55:13 -0400

    Configure wbrs server in config.yml.

--- app/models/wbrs/base.rb
+++ app/models/wbrs/base.rb
@@ -1,6 +1,26 @@
 class Wbrs::Base
   include ActiveModel::Model
 
+  def self.host
+    @host ||= Rails.configuration.wbrs.host
+  end
+
+  def self.port
+    @port ||= Rails.configuration.wbrs.port
+  end
+
+  def self.tls_mode
+    @tls_mode ||= Rails.configuration.wbrs.tls_mode
+  end
+
+  def self.gssnegotiate?
+    @gssnegotiate ||= Rails.configuration.wbrs.gssnegotiate
+  end
+
+  def self.ca_file
+    @ca_cert_file ||= Rails.configuration.wbrs.ca_cert_file
+  end
+
   def self.stringkey_params(conditions = {})
     conditions.inject({}) do |params, (key, value)|
       params[key.to_s] = value if value
@@ -11,15 +31,30 @@ class Wbrs::Base
   def self.request(path:, body:)
     raise 'Path required' unless path.present?
     raise 'Path must start with slash (/)' unless '/' == path[0]
-    request = HTTPI::Request.new("http://localhost:2944#{path}")
-    # request.url = "http://localhost:2944#{path}"
 
-    # request.auth.ssl.verify_mode = :peer
-    # request.auth.ssl.ca_cert_file = ca_file #this will be nil for Heroku apps
-    # request.auth.gssnegotiate
-    # if request.auth.basic?
-    #   request.auth.basic(@basic_auth[:user], @basic_auth[:password])
-    # end
+    protocol =
+        case tls_mode
+          when 'verify-peer'
+            'https'
+          when 'verify-none'
+            'https'
+          else #no-tls
+            'http'
+        end
+
+    request = HTTPI::Request.new("#{protocol}://#{host}:#{port}#{path}")
+
+    case tls_mode
+      when 'verify-peer'
+        request.ssl = true
+        request.auth.ssl.verify_mode = :peer
+        request.auth.ssl.ca_cert_file = ca_cert_file #this will be nil for Heroku apps
+      when 'verify-none'
+        request.ssl = true
+        request.auth.ssl.verify_mode = :none
+      else #no-tls
+        request.ssl = false
+    end
 
     request.headers = {"Content-Type" => "application/json" }
     request.body = body.to_json
@@ -27,12 +62,19 @@ class Wbrs::Base
   end
 
   def self.make_get_request(path:, body:)
-    HTTPI.get(request(path: path, body: body))
+    if gssnegotiate?
+      HTTPI.get(request(path: path, body: body), :curb)
+    else
+      HTTPI.get(request(path: path, body: body))
+    end
   end
 
-
   def self.make_post_request(path:, body:)
-    HTTPI.post(request(path: path, body: body))
+    if gssnegotiate?
+      HTTPI.post(request(path: path, body: body), :curb)
+    else
+      HTTPI.post(request(path: path, body: body))
+    end
   end
 
   def self.request_error_handling(response)
