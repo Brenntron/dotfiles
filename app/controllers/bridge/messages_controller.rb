40cd8470c3134821fb36513addbc58c00c9d02ef
Chris LaClair <claclair@cisco.com>
2018-06-09 22:52:49 -0400

    #1814: troubleshooting

--- app/controllers/bridge/messages_controller.rb
+++ app/controllers/bridge/messages_controller.rb
@@ -22,7 +22,9 @@ class Bridge::MessagesController < ApplicationController
         puts message_params.inspect
         puts "\n"
         puts message_params.keys.first
-        puts "\n"
+        puts "\n%%%%%%%%%%%%%\n"
+        puts message_params[:dispute_email].inspect
+        puts "\n%%%%%%%%%%%%%\n"
         obj_type_key = message_params.keys.first
         obj_type = obj_type_key.to_s.camelize
         puts obj_type_key
