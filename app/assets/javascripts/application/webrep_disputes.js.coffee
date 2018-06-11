b074e616c78e6b2498721a7f862a466c324fbf1c
abreeman <abreeman@cisco.com>
2018-06-11 16:46:39 -0400

    closes #1835 MF index page

--- app/assets/javascripts/application/webrep_disputes.js.coffee
+++ app/assets/javascripts/application/webrep_disputes.js.coffee
@@ -22,3 +22,25 @@ window.populate_webrep_index_table = () ->
       #$("#create_research_submit_wait").addClass('hidden').hide()
       #$("#create_research_submit").show()
   , this)
+
+window.populate_webrep_my_table = () ->
+  headers = {'Token': $('input[name="token"]').val(), 'Xmlrpc-Token': $('input[name="xml_token"]').val()}
+  $.ajax(
+    url: '/api/v1/escalations/webrep_disputes/disputes/my'
+    method: 'GET'
+    headers: headers
+    success: (response) ->
+
+      json = $.parseJSON(response)
+      if json.error
+        notice_html = "<p>Something went wrong: #{json.error}</p>"
+        alert(json.error)
+      else
+        datatable = $('#my-disputes').DataTable()
+        datatable.clear();
+        datatable.rows.add(json.data);
+        datatable.draw();
+
+    error: (response) ->
+      notice_html = "<p>Something went wrong: #{response.responseText}</p>"
+  , this)
--- app/controllers/api/v1/escalations/webrep_disputes/disputes.rb
+++ app/controllers/api/v1/escalations/webrep_disputes/disputes.rb
@@ -22,12 +22,44 @@ module API
                 dispute_packet[:id] = dispute.id
 
                 # dispute_packet[:case_number] = dispute.case_number
-                dispute_packet[:case_number] = sprintf '%08d', dispute.id
+                # dispute_packet[:case_number] = sprintf '%08d', dispute.id
                 dispute_packet[:customer_name] = dispute.customer_name
                 dispute_packet[:customer_company_name] = dispute.org_domain # should be: dispute.customer_company_name
                 dispute_packet[:status] = dispute.status.upcase
                 dispute_packet[:assigned_to] = dispute.assignee
                 dispute_packet[:actions] = "<a href='/escalations/webrep/disputes/#{dispute.id}'>edit</a>"
+                dispute_packet[:case_number] = "<a href='/escalations/webrep/disputes/#{dispute.id}'>" + ( sprintf '%08d', dispute.id ).to_s + "</a>"
+
+                dispute_packet[:case_opened_at] = dispute.case_opened_at.strftime('%Y-%m-%d %H:%M:%S')
+                dispute_packet[:case_age] = dispute.dispute_age
+                # dispute_packet[:suggested_disposition] = 'Malicious: Phishing'
+                dispute_packet[:suggested_disposition] = dispute.suggested_d
+                dispute_packet[:priority] = dispute.priority
+                dispute_packet[:source] = dispute.ticket_source
+                dispute_packet[:source_id] = dispute.ticket_source_key
+
+                json_packet << dispute_packet
+              end
+              {:status => "success", :data => json_packet}.to_json
+
+            end
+
+            get "my" do
+
+              json_packet = []
+
+              disputes = Dispute.where("id like '20%18'") # random subset, for now
+
+              disputes.each do |dispute|
+                dispute_packet = {}
+                dispute_packet[:id] = dispute.id
+
+                dispute_packet[:customer_name] = dispute.customer_name
+                dispute_packet[:customer_company_name] = dispute.org_domain # should be: dispute.customer_company_name
+                dispute_packet[:status] = dispute.status.upcase
+                dispute_packet[:assigned_to] = dispute.assignee
+                dispute_packet[:actions] = "<a href='/escalations/webrep/disputes/#{dispute.id}'>edit</a>"
+                dispute_packet[:case_number] = "<a href='/escalations/webrep/disputes/#{dispute.id}'>" + ( sprintf '%08d', dispute.id ).to_s + "</a>"
 
                 dispute_packet[:case_opened_at] = dispute.case_opened_at.strftime('%Y-%m-%d %H:%M:%S')
                 dispute_packet[:case_age] = dispute.dispute_age
