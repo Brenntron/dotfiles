58430b1bcfe8ce6af95f86b9afee190be8f76584
NickHerbert <nherbert@sourcefire.com>
2018-06-19 16:47:00 -0400

    removing escalation.rb and modifying escalation_link.rb

--- app/models/escalation_link.rb
+++ app/models/escalation_link.rb
@@ -1,4 +1,8 @@
 class EscalationLink < ApplicationRecord
   belongs_to :snort_research_bug, class_name: "Bug"
   belongs_to :snort_escalation_bug, class_name: "Bug"
+  
+  def process_bridge_payload
+
+  end
 end
