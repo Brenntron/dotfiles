15369b267a447f9a86b38356a91991ba8361012d
Marlin W. Pierce <marlpier@cisco.com>
2018-06-22 13:59:42 -0400

    Implement save! with staging web server.

--- app/models/rep_api/blacklist.rb
+++ app/models/rep_api/blacklist.rb
@@ -1,16 +1,17 @@
 class RepApi::Blacklist < RepApi::Base
   FIELD_NAMES = %w{entry disposition public excluded classifications manual_classifications class_id
-                   expiration hostname author primary_source metadata seen_by comment
-                   rev first_seen last_seen stale}
+                   expiration hostname author primary_source metadata seen_by
+                   _id _rev first_seen last_seen stale status
+                   message}
   FIELD_SYMS = FIELD_NAMES.map{|name| name.to_sym}
 
   attr_accessor *FIELD_SYMS
 
-  validates :entry, presence: true
+  validates :entry, :classifications, presence: true
 
   def initialize(attributes = {})
+    @new_record = attributes.has_key?(:new_record) ? attributes.delete(:new_record) : true
     super
-    @new_record = true
   end
 
   def new_record?
@@ -38,11 +39,12 @@ class RepApi::Blacklist < RepApi::Base
   end
 
   def attributes
-    { entry: author,
-      author: author,
-      public: public?,
-      excluded: excluded?,
-      comment: comment }
+    {
+        entry: author,
+        public: public?,
+        excluded: excluded?,
+        comment: comment
+    }
   end
 
   # Get the blacklist entries from the reputation API
@@ -54,9 +56,9 @@ class RepApi::Blacklist < RepApi::Base
     entries = params.delete('entries')
     raise 'Missing required entries condition' unless entries
     return [] unless entries.present?
-    body = entries.map {|entry| "entry=#{entry}"}.join('&')
+    string_array = entries.map {|entry| "entry=#{entry}"}
 
-    response = call_json_request(:post, '/blacklist/get', body: body)
+    response = call_json_request(:post, '/blacklist/get', body: build_request_body(string_array))
 
     response_body = JSON.parse(response.body)
     response_body.inject({}) do |collection_hash, (entry, value)|
@@ -71,17 +73,52 @@ class RepApi::Blacklist < RepApi::Base
     []
   end
 
-  def save!
+  # Save the blacklist object.
+  # To add or update a blacklist entry, set the entry and classifications fields on a Blacklist object,
+  # and call this method.
+  # The entry field may be an array.
+  # @param [String] author moniker of who is adding or updating this entry.
+  # @param [String] comment
+  # @return [Array<RepApi::Blacklist>] collection of responses with entry, expiration, and message.
+  def save!(params = {})
     raise "Validation failed: #{errors.full_messages.join(', ')}" unless valid?
 
-    if new_record?
-      call_json_request(:post, '/blacklist/add', body: stringkey_params(attributes))
-      @new_record = false
-      true
-    else
-      call_json_request(:post, '/blacklist/add', body: stringkey_params(attributes))
-      @new_record = false
-      true
+    input = stringkey_params(params)
+    raise "Missing parameter: author" unless input.has_key?('author')
+    raise "Missing parameter: comment" unless input.has_key?('comment')
+
+    input = input.to_a
+    input += self.classifications.map{ |classification| "classification=#{classification}" }
+    entries = entry.kind_of?(Array) ? entry : [entry]
+    input += entries.map{ |entry_curr| "entry=#{entry_curr}" }
+
+    response = call_json_request(:post, '/blacklist/add', body: build_request_body(input))
+    @new_record = false
+    blacklist_hash = JSON.parse(response.body).inject({}) do |hash, message|
+      contained_entry = entries.find{ |entry| message['MSG'].include?(entry) }
+      case
+        when message['expiration'].present?
+          hash[message['entry']] = RepApi::Blacklist.new(entry: message['entry'],
+                                                         expiration: message['expiration'],
+                                                         message: message['MSG'],
+                                                         new_record: false)
+        when contained_entry
+          hash[contained_entry] = RepApi::Blacklist.new(entry: contained_entry,
+                                                        message: message['MSG'],
+                                                        new_record: true)
+      end
+
+      hash
+    end
+
+    entries.map do |entry_curr|
+      key = blacklist_hash.keys.find{ |key_curr| entry_curr.include?(key_curr) }
+      if key
+        blacklist_hash[key]
+      else
+        RepApi::Blacklist.new(entry: entry_curr,
+                              new_record: true)
+      end
     end
   end
 
