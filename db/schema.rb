b8e85c85ab18f82a5b8d115ebd7ab3f5278aa6b8
Nicolette Verbeck <nverbeck@cisco.com>
2018-06-26 16:52:11 -0400

    #1952 schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 20180621224342) do
     t.index ["task_id"], name: "index_attachments_on_task_id"
   end
 
+  create_table "attachments_exploits", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "attachment_id"
+    t.integer "exploit_id"
+  end
+
+   create_table "attachments_rules", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+     t.integer "attachment_id"
+     t.integer "rule_id"
+   end
+
   create_table "bug_blockers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "snort_blocker_bug_id"
     t.integer "snort_blocked_bug_id"
@@ -98,8 +108,8 @@ ActiveRecord::Schema.define(version: 20180621224342) do
     t.string "liberty", default: "CLEAR"
     t.string "whiteboard"
     t.boolean "acknowledged"
-    t.boolean "snort_secure", default: false
     t.datetime "due_date"
+    t.boolean "snort_secure", default: false
     t.string "type", default: "ResearchBug"
     t.index ["user_id"], name: "index_bugs_on_user_id"
   end
@@ -474,8 +484,15 @@ ActiveRecord::Schema.define(version: 20180621224342) do
     t.integer "reference_type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "fail_count", default: 0
+    t.integer "fail_count"
     t.index ["reference_type_id"], name: "index_references_on_reference_type_id"
+  end
+
+  create_table "references_rules", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "reference_id"
+    t.integer "rule_id"
+    t.index ["reference_id"], name: "index_references_rules_on_reference_id"
+    t.index ["rule_id"], name: "index_references_rules_on_rule_id"
   end
 
   create_table "roles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -501,7 +518,7 @@ ActiveRecord::Schema.define(version: 20180621224342) do
     t.text "summary"
     t.text "impact"
     t.text "details"
-    t.text "affected_sys"
+    t.text "affected_sys", limit: 4294967295
     t.text "attack_scenarios"
     t.text "ease_of_attack"
     t.text "false_positives"
