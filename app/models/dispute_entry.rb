06443bf4f80dba14c9968e82ed9f881a6f2eef8f
Marlin W. Pierce <marlpier@cisco.com>
2018-06-27 15:41:14 -0400

    Add tentative hostlookup method.

--- app/models/dispute_entry.rb
+++ app/models/dispute_entry.rb
@@ -2,4 +2,8 @@ class DisputeEntry < ApplicationRecord
   has_paper_trail on: [:update], ignore: [:updated_at]
   belongs_to :dispute
   has_many :dispute_rule_hits
+
+  def hostlookup
+    self.uri || self.ip_address
+  end
 end
