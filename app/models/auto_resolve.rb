f6035af84cdf106e6c18de47335882b2f0407ada
Marlin W. Pierce <marlpier@cisco.com>
2018-07-03 12:37:53 -0400

    check complaints

--- app/models/auto_resolve.rb
+++ app/models/auto_resolve.rb
@@ -1,5 +1,5 @@
 class AutoResolve < ActiveModel
-  attr_accessor :address_type, :address, :status
+  attr_accessor :address_type, :address, :status, :rule_hits
 
   ADDRESS_TYPE_IP           = 'IP'
   ADDRESS_TYPE_URI          = 'URI'
@@ -28,8 +28,14 @@ class AutoResolve < ActiveModel
     STATUS_MALICIOUS == self.status
   end
 
-  def check_complaints
+  def good_mnem?(rule_hit)
+    %w{tuse a500 vsvd suwl wlw wlm wlh deli ciwl beaker_drl}.include(rule_hit.mnem)
+  end
 
+  def check_complaints(rule_hits:)
+    if rule_hits&.any? && rule_hits.find{|rule_hit| good_mnem?(rule_hit)}
+      self.status = STATUS_NEW
+    end
   end
 
   def check_virus_total
@@ -42,7 +48,7 @@ class AutoResolve < ActiveModel
 
   def check_sources
     if Rails.configuration.check_complaints
-      check_complaints
+      check_complaints(rule_hits: self.rule_hits)
       return if self.status
     end
 
@@ -61,7 +67,8 @@ class AutoResolve < ActiveModel
 
   # @param [String] address_type: 'IP' or 'URI/DOMAIN'
   # @param [String] address: ip address, uri, or domain
-  def self.create_from_payload(address_type:, address:)
+  # @param [Array<TBD>] rule_hits: collection of our rule hits
+  def self.create_from_payload(address_type:, address:, rule_hits: nil)
     address_type_attr =
         case address_type
           when 'IP'
@@ -72,7 +79,7 @@ class AutoResolve < ActiveModel
             ADDRESS_TYPE_DOMAIN
         end
 
-    auto_resolve = new(address_type: address_type_attr, address: address)
+    auto_resolve = new(address_type: address_type_attr, address: address, rule_hits: rule_hits)
     auto_resolve.check_sources
     auto_resolve
   end
