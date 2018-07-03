c816955bdd3430a05fd68b19496976c2954e06ce
micah <mimcfarl@cisco.com>
2018-07-03 10:31:19 -0400

    Adds virustotal api calls

--- config/initializers/config_yml.rb
+++ config/initializers/config_yml.rb
@@ -78,4 +78,11 @@ Rails.configuration.xbrs.port           = xbrs['port']
 Rails.configuration.xbrs.tls_mode       = xbrs['tls_mode']
 Rails.configuration.xbrs.gssnegotiate   = xbrs['gssnegotiate']
 
+virustotal = env_config.fetch('virustotal', {})
+Rails.configuration.virustotal          = OpenStruct.new
+Rails.configuration.virustotal.host     = virustotal['host']
+Rails.configuration.virustotal.port     = virustotal['port']
+Rails.configuration.virustotal.api_key  = virustotal['api_key']
+
+
 
