b4f7567fb4dcce98916008abec98a58c7a99424a
Melissa T <melissaltaylor@gmail.com>
2018-07-10 12:06:49 -0400

    Shortened number of results on research page for quicker temporary load. Moved over updated data calls into table.

--- app/controllers/escalations/webrep/disputes_controller.rb
+++ app/controllers/escalations/webrep/disputes_controller.rb
@@ -25,7 +25,7 @@ class Escalations::Webrep::DisputesController < ApplicationController
 
   def research
     # This is temporary till we get the searched hooked up
-    @entries = DisputeEntry.all.limit(50)
+    @entries = DisputeEntry.all.limit(5)
   end
 
   def tickets
