5d0c8a5ecf23b3661c54a6b48cd297c16fd4e778
Marlin W. Pierce <marlpier@cisco.com>
2018-07-12 15:10:31 -0400

    Total row on HTML

--- app/models/webcat_report/resolution_report.rb
+++ app/models/webcat_report/resolution_report.rb
@@ -13,6 +13,29 @@ class WebcatReport::ResolutionReport
   end
 
   def each_engineer
+
+    times_select_phrase =
+        'avg(time_to_sec(timediff( complaint_closed_at, complaint_assigned_at ))) as eng_avg, ' +
+            'max( time_to_sec( timediff( complaint_closed_at, complaint_assigned_at ))) as eng_max, ' +
+            'avg(time_to_sec(timediff( complaint_closed_at, created_at ))) as dept_avg, ' +
+            'max( time_to_sec( timediff( complaint_closed_at, created_at ))) as dept_max'
+
+    counts = Complaint.where(complaint_closed_at: (@date_from..@date_to+1)).group(:resolution).count
+
+    times =
+        Complaint.where.not(complaints: {resolution: nil})
+            .where(complaints: {complaint_closed_at: (@date_from..@date_to+1)})
+            .select(times_select_phrase).first
+
+    yield ResolutionCount.new(username: 'TOTAL',
+                              fixed: counts[Complaint::RESOLUTION_FIXED],
+                              invalid: counts[Complaint::RESOLUTION_INVALID],
+                              unchanged: counts[Complaint::RESOLUTION_UNCHANGED],
+                              duplicate: counts[Complaint::RESOLUTION_DUPLICATE],
+                              eng_avg: times.eng_avg, eng_max: times.eng_max,
+                              dept_avg: times.dept_avg, dept_max: times.dept_max)
+
+
     User.joins(:complaints)
         .where.not(complaints: {resolution: nil})
         .where(complaints: {complaint_closed_at: (@date_from..@date_to+1)})
@@ -21,15 +44,10 @@ class WebcatReport::ResolutionReport
       counts =
         Complaint.where(user_id: user.id, complaint_closed_at: (@date_from..@date_to+1)).group(:resolution).count
 
-      select =
-        'avg(time_to_sec(timediff( complaint_closed_at, complaint_assigned_at ))) as eng_avg, ' +
-          'max( time_to_sec( timediff( complaint_closed_at, complaint_assigned_at ))) as eng_max, ' +
-          'avg(time_to_sec(timediff( complaint_closed_at, created_at ))) as dept_avg, ' +
-          'max( time_to_sec( timediff( complaint_closed_at, created_at ))) as dept_max'
       times =
         Complaint.where.not(complaints: {resolution: nil})
           .where(user_id: user.id, complaints: {complaint_closed_at: (@date_from..@date_to+1)})
-          .select(select).first
+          .select(times_select_phrase).first
 
       yield ResolutionCount.new(username: user.cvs_username,
                                 fixed: counts[Complaint::RESOLUTION_FIXED],
