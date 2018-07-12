2a320cddb0d3b59196ebd90cd842b00cf275a45f
Marlin W. Pierce <marlpier@cisco.com>
2018-07-12 13:59:51 -0400

    Closes #2010, Webcat Report.

--- app/controllers/escalations/webcat/reports_controller.rb
+++ app/controllers/escalations/webcat/reports_controller.rb
@@ -7,4 +7,17 @@ class Escalations::Webcat::ReportsController < Escalations::WebcatController
     @report = WebcatReport::ResolutionReport.new(date_from: params['date_from'],
                                                  date_to: params['date_to'])
   end
+
+  def export_resolution
+    @report = WebcatReport::ResolutionReport.new(date_from: params['date_from'],
+                                                 date_to: params['date_to'])
+
+    contents = CSV.generate do |csv|
+      csv << [ '', 'Fixed Complaints', 'Invalid Complaints', 'Unchanged Complaints', 'Duplicate Complaints' ]
+      @report.each_engineer do |counts|
+        csv << [ counts.username, counts.fixed, counts.invalid, counts.unchanged, counts.duplicate ]
+      end
+    end
+    send_data contents
+  end
 end
