05476eb6061d13f5b25008a3dbbed25fcedb1079
Marlin W. Pierce <marlpier@cisco.com>
2018-07-12 15:07:07 -0400

    Times on HTML report.

--- app/models/webcat_report/resolution_report.rb
+++ app/models/webcat_report/resolution_report.rb
@@ -1,7 +1,8 @@
 class WebcatReport::ResolutionReport
   class ResolutionCount
     include ActiveModel::Model
-    attr_accessor :username, :fixed, :invalid, :unchanged, :duplicate
+    attr_accessor :username, :fixed, :invalid, :unchanged, :duplicate,
+                  :eng_avg, :eng_max, :dept_avg, :dept_max
   end
 
   attr_reader :date_from, :date_to
@@ -16,12 +17,27 @@ class WebcatReport::ResolutionReport
         .where.not(complaints: {resolution: nil})
         .where(complaints: {complaint_closed_at: (@date_from..@date_to+1)})
         .group(:id).order(:cvs_username).each do |user|
-      counts = Complaint.where(user_id: user.id, complaint_closed_at: (@date_from..@date_to+1)).group(:resolution).count
+
+      counts =
+        Complaint.where(user_id: user.id, complaint_closed_at: (@date_from..@date_to+1)).group(:resolution).count
+
+      select =
+        'avg(time_to_sec(timediff( complaint_closed_at, complaint_assigned_at ))) as eng_avg, ' +
+          'max( time_to_sec( timediff( complaint_closed_at, complaint_assigned_at ))) as eng_max, ' +
+          'avg(time_to_sec(timediff( complaint_closed_at, created_at ))) as dept_avg, ' +
+          'max( time_to_sec( timediff( complaint_closed_at, created_at ))) as dept_max'
+      times =
+        Complaint.where.not(complaints: {resolution: nil})
+          .where(user_id: user.id, complaints: {complaint_closed_at: (@date_from..@date_to+1)})
+          .select(select).first
+
       yield ResolutionCount.new(username: user.cvs_username,
                                 fixed: counts[Complaint::RESOLUTION_FIXED],
                                 invalid: counts[Complaint::RESOLUTION_INVALID],
                                 unchanged: counts[Complaint::RESOLUTION_UNCHANGED],
-                                duplicate: counts[Complaint::RESOLUTION_DUPLICATE])
+                                duplicate: counts[Complaint::RESOLUTION_DUPLICATE],
+                                eng_avg: times.eng_avg, eng_max: times.eng_max,
+                                dept_avg: times.dept_avg, dept_max: times.dept_max)
     end
   end
 end
