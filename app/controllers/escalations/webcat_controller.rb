4a1a258ffd58bbc9a1147da0bc511dfa9782ba7e
NickHerbert <nherbert@sourcefire.com>
2018-07-13 16:39:38 -0400

    local telemetry data hooked up

--- app/controllers/escalations/webcat_controller.rb
+++ app/controllers/escalations/webcat_controller.rb
@@ -3,15 +3,17 @@ class Escalations::WebcatController < ApplicationController
   before_action :dashboard_metrics
 
   def dashboard_metrics
-    @secHub_1 = 62
-    @secHub_2 = 2827
+    @secHub_1 = 0
+    @secHub_2 = 0
     @ss = 0
     @int_1 = 0
     @int_2 = 0
-    @wbnp = 37
+    @wbnp = 0
 
-    @pending = 25
-    @new = 25
-    @overdue = 0
+    @assigned = ComplaintEntry.assigned_count
+    @pending = ComplaintEntry.pending_count
+    @new = ComplaintEntry.new_count
+    @overdue = ComplaintEntry.overdue_count
+
   end
 end
--- app/models/complaint_entry.rb
+++ app/models/complaint_entry.rb
@@ -4,6 +4,11 @@ class ComplaintEntry < ApplicationRecord
   belongs_to :complaint
   belongs_to :user, optional: true
 
+  scope :assigned_count , -> {where(status:"ASSIGNED").count}
+  scope :pending_count , -> {where(status:"PENDING").count}
+  scope :new_count , -> {where(status:"NEW").count}
+  scope :overdue_count , -> {where("created_at < ?",Time.now - 24.hours).count}
+
   def self.what_time_is_it(value)
     distance_of_time_in_words(value)
   end
@@ -14,12 +19,12 @@ class ComplaintEntry < ApplicationRecord
 
   def take_complaint(current_user)
     if user.nil?
-      self.update(user:current_user)
+      self.update(user:current_user, status:"ASSIGNED")
     end
   end
   def return_complaint(current_user)
     if self.user == current_user
-      self.update(user:nil)
+      self.update(user:nil, status:"NEW")
     end
   end
 end
