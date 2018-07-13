906d8535853836c1776253180473c13d78d81fac
Marlin W. Pierce <marlpier@cisco.com>
2018-07-13 14:09:33 -0400

    Export CSV

--- app/controllers/escalations/webrep/disputes_controller.rb
+++ app/controllers/escalations/webrep/disputes_controller.rb
@@ -3,10 +3,31 @@ class Escalations::Webrep::DisputesController < ApplicationController
   before_action :require_login
 
   def index
-    @disputes = Dispute.robust_search(params.fetch(:dispute, {})['search_type'],
-                                      search_name: params.fetch(:dispute, {})['search_name'],
-                                      params: index_params,
-                                      user: current_user)
+    respond_to do |format|
+      format.html
+      format.csv do
+        @disputes = Dispute.robust_search(params.fetch(:dispute, {})['search_type'],
+                                          search_name: params.fetch(:dispute, {})['search_name'],
+                                          params: index_params,
+                                          user: current_user)
+        contents = CSV.generate do |csv|
+          csv << [ 'Priority', 'Case ID', 'Status', 'Dispute', 'Count', 'Owner', 'Time Submitted', 'Age' ]
+          @disputes.each do |dispute|
+            csv << [
+                'P3',
+                dispute.case_number,
+                dispute.status,
+                dispute.org_domain,
+                dispute.dispute_entries.count,
+                dispute.user.cvs_username,
+                dispute.case_opened_at,
+                ApplicationRecord.humanize_secs(Time.now - dispute.case_opened_at)
+            ]
+          end
+        end
+        send_data contents
+      end
+    end
   end
 
   def show
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,27 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.humanize_secs(sec_input)
+    mm, ss = sec_input.to_i.divmod(60)
+    hh, mm = mm.divmod(60)
+    dd, hh = hh.divmod(24)
+    case
+      when 366 < dd
+        "#{dd / 365} years"
+      when 56 <= dd
+        "#{dd / 30} months"
+      when 21 <= dd
+        "#{dd / 7} weeks"
+      when dd > 0
+        "%id %ih" % [dd, hh]
+      when hh > 0
+        "%ih %im" % [hh, mm]
+      else
+        "%im %is" % [mm, ss]
+    end
+  end
+
+  def humanize_secs(sec_input)
+    ApplicationRecord.humanize_secs(sec_input)
+  end
 end
