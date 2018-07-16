951de6db71fab20357c4e394616ee03260cc0bab
micah <mimcfarl@cisco.com>
2018-07-16 06:46:38 -0400

    begins grape route for email retrieval

---
+++ app/controllers/api/v1/rulehit_resolution_mailer_templates.rb
@@ -0,0 +1,38 @@
+module API
+  module V1
+    class RulehitResolutionMailerTemplates < Grape::API
+      include API::V1::Defaults
+
+      resource :rulehit_resolution_mailer_templates do
+        # before do
+        #   PaperTrail.whodunnit = current_user.id if current_user.present?
+        # end
+
+        desc "get a mailer template corresponding to a rulehit"
+        params do
+          requires :rulehit_id, type: Integer, desc: "Bugzilla id."
+        end
+        get 'make_rulehit_mail/:rulehit_id' do
+          # authorize!(:import, ResearchBug)
+
+          Rails.logger.debug("Retrieving rulehit mailer template...")
+          # progress_bar = Event.create(user: current_user.display_name, action: "import_bug:#{params[:id]}", description: "#{request.headers["Token"]}", progress: 10)
+
+          begin
+            return "Hello from Grape!"
+
+          rescue Exception => e
+            Rails.logger.error "Something went wrong! Failed to retrieve template."
+            Rails.logger.error $!
+            Rails.logger.error $!.backtrace.join("\n")
+            error = "Something went wrong! Failed to retrieve template."
+            {:error => error}.to_json
+          end
+
+        end
+
+
+      end
+    end
+  end
+end
