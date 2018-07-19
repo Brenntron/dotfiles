230f755ad5abe0009bde6573ebb47bb026fc2f4a
micah <mimcfarl@cisco.com>
2018-07-19 16:53:10 -0400

    Displays current assignee correctly

--- app/controllers/api/v1/escalations/webrep/disputes.rb
+++ app/controllers/api/v1/escalations/webrep/disputes.rb
@@ -76,7 +76,6 @@ module API
                 end
 
                 dispute_packet[:dispute_entries] = dispute.dispute_entries
-
                 dispute_packet[:d_entry_preview] = "<span class='dispute_entry_content_first'>" + dispute_packet[:dispute_entry_content].first.to_s + "</span><span class='dispute-count'>" + dispute_packet[:dispute_count] + "</span>"
                 dispute_packet[:status] = dispute.status
                 dispute_packet[:resolution] = dispute.resolution
@@ -84,6 +83,11 @@ module API
                 if dispute.assignee == 'Unassigned'
                   dispute_packet[:assigned_to] = "<span class='missing-data'>Unassigned</span><button class='take-ticket-button' title='Assign this ticket to me'></button>"
                 end
+
+                if dispute.user_id?
+                  dispute_packet[:assigned_to] = User.find(dispute.user_id).cvs_username + " <button class='take-ticket-button' title='Assign this ticket to me'></button>"
+                end
+
                 dispute_packet[:actions] = "<a href='/escalations/webrep/disputes/#{dispute.id}'>edit</a>"
 
                 dispute_packet[:case_opened_at] = dispute.case_opened_at&.strftime('%Y-%m-%d %H:%M:%S')
@@ -137,7 +141,7 @@ module API
               json_packet = []
               params[:dispute_ids].each do |dispute|
                 d = Dispute.find_by(id: dispute)
-                d.update(user_id: params[:new_assignee])
+                d.update!(user_id: params[:new_assignee])
                 json_packet << d
               end
               {:status => "success", :data => json_packet}.to_json
