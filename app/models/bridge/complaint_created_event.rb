6686c4835847a4028c2f8d82fab08327c26219cd
Chris LaClair <claclair@cisco.com>
2018-07-21 15:34:21 -0400

    #1814, some small fixes to disputes, first round of successful cat complaint bridge origination

---
+++ app/models/bridge/complaint_created_event.rb
@@ -0,0 +1,16 @@
+class Bridge::ComplaintCreatedEvent < Bridge::BaseMessage
+  def initialize(addressee:, source_authority: nil, source_key: nil)
+    super(channel: 'ticket-acknowledge',
+          addressee: addressee)
+    @source_authority = source_authority
+    @source_key = source_key
+  end
+
+  def post(payload, source_authority: @source_authority, source_key: @source_key)
+    super(message: {source_authority: source_authority,
+                    source_key: source_key,
+                    ticket_entries: payload,
+                    ac_status: Complaint::AC_SUCCESS
+                    })
+  end
+end
