1b5e8282c71411a34ccd61f69f4aeb8b23859a99
Marlin W. Pierce <marlpier@cisco.com>
2018-07-24 14:53:26 -0400

    Closes #2085, MF UI - Show Page: Ticket Status

--- app/controllers/api/v1/escalations/webrep/disputes.rb
+++ app/controllers/api/v1/escalations/webrep/disputes.rb
@@ -205,6 +205,17 @@ module API
 
               { username: current_user.display_name, dispute_ids: dispute_ids }
             end
+
+            params do
+              requires :status, type: String
+            end
+            patch 'entries/:entry_id/status' do
+              authorize!(:update, DisputeEntry)
+              entry = DisputeEntry.find(params['entry_id'])
+              authorize!(:update, entry)
+              entry.update(status: permitted_params['status'])
+              true
+            end
           end
         end
       end
