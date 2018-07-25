c0e1d127fd8e16a8522f04c3f85a71cc8eca7572
micah <mimcfarl@cisco.com>
2018-07-25 15:05:51 -0400

    Fixes some typos, sets user id correctly

--- app/controllers/api/v1/escalations/webrep/disputes.rb
+++ app/controllers/api/v1/escalations/webrep/disputes.rb
@@ -143,7 +143,9 @@ module API
             post "new_adhoc_entry" do
               json_packet = []
 
-              entry = DisputeEntry.new(:dispute_id => params[:dispute_id], user_id => current_user.id)
+              user = Dispute.find(params[:dispute_id]).user_id
+
+              entry = DisputeEntry.new(:dispute_id => params[:dispute_id], :user_id => user)
 
               is_ip_address = !!(params[:uri]  =~ Resolv::IPv4::Regex)
 
@@ -152,12 +154,11 @@ module API
               else
                 entry.uri = params[:uri]
               end
-              binding.pry
-              entry.save!
+              entry.save
 
               json_packet << entry
 
-              {:status => "success", :data => json_packet}.to_json if entry.save?
+              {:status => "success", :data => json_packet}.to_json if entry.save
             end
 
             desc "Change assignee of a group of dispute IDs"
