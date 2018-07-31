9ec76ee6264cdb56d91d3ccc708141e8813fbf80
Marlin W. Pierce <marlpier@cisco.com>
2018-07-31 11:37:21 -0400

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726200023) do
 ActiveRecord::Schema.define(version: 20180730165632) do
 
   create_table "alerts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
