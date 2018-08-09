ed7314a0505dc7aba20446d186ec9677cceee4f3
Marlin W. Pierce <marlpier@cisco.com>
2018-08-09 13:28:54 -0400

    naming convention

--- db/migrate/20180809144852_add_at_to_complaint_entries.rb
+++ db/migrate/20180809144852_add_at_to_complaint_entries.rb
@@ -1,6 +1,6 @@
 class AddAtToComplaintEntries < ActiveRecord::Migration[5.1]
   def change
-    add_column :complaint_entries, :complaint_assigned_at, :datetime
-    add_column :complaint_entries, :complaint_closed_at, :datetime
+    add_column :complaint_entries, :complaint_entry_assigned_at, :datetime
+    add_column :complaint_entries, :complaint_entry_closed_at, :datetime
   end
 end
