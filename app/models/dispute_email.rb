ee89d34b7a6a8d3aa06493d80c99f46931afede4
abreeman <abreeman@cisco.com>
2018-08-21 16:37:19 -0400

    closes #2527; not shown: config and debug with @nherbert

--- app/models/dispute_email.rb
+++ app/models/dispute_email.rb
@@ -2,7 +2,7 @@ class DisputeEmail < ApplicationRecord
   belongs_to :dispute
   has_many :dispute_email_attachments
 
-  EMAIL_DOMAIN = "dispute.talosintelligence.com"
+  EMAIL_DOMAIN = "email.talosintelligence.com"
   NOREPLY      = "noreply"
 
   UNREAD   = "unread"
@@ -31,7 +31,7 @@ class DisputeEmail < ApplicationRecord
       bad_email_args[:body] = bad_gateway_body
 
       attachments_to_mail = []
-      conn = ::Bridge::SendEmailEvent.new(addressee: 'talos-intelligence', source_authority: 'talos-intelligence')
+      conn = ::Bridge::SendEmailEvent.new(addressee: 'talos-intelligence')
       conn.post(bad_email_args, attachments_to_mail)
 
       conn = ::Bridge::EmailCreatedEvent.new(addressee: "talos-intelligence", source_authority: "talos-intelligence", source_key: message_payload["source_key"])
@@ -191,14 +191,13 @@ class DisputeEmail < ApplicationRecord
   ## AUTO EMAIL MANAGEMENT
 
   def self.bad_gateway_subject
-    "THINGS"
+    "Unable to process dispute submission"
   end
 
   def self.bad_gateway_body
-    <<-BADGATEWAY
-
-      THIS IS THE BODY OF A BAD GATEWAY EMAIL THAT INSTRUCTS HOW TO USE TALOS INTELLIGENCE TO START A NEW CASE
-
+    <<~BADGATEWAY
+Thank you for emailing the Cisco Talos Intelligence Group dispute system.  Unfortunately, we are not able to accommodate a request sent directly to the system.  In order to file a dispute against a category or reputation score, please submit your request via our dispute page: 
+https://talosintelligence.com/reputation_center/support
     BADGATEWAY
   end
 
