d851b47e64c8d27504cbf99a0906f718fd2f2693
Marlin W. Pierce <marlpier@cisco.com>
2018-09-05 16:50:31 -0400

    Use strong params for test.

--- spec/models/complaint_spec.rb
+++ spec/models/complaint_spec.rb
@@ -1,32 +1,32 @@
 describe Complaint do
   let(:complaint_message_payload) do
-    {
-        source_key: 49,
-        source_type: "Complaint",
-        payload: {
-            name: "Marlin Pierce",
-            email: "marlpier@cisco.com",
-            domain: "cisco.com",
-            problem: "New category",
-            details: '',
-            user_ip: "::1",
-            ticket_time: "September 03, 2018 12:05",
-            investigate_ips: {},
-            investigate_urls: {
-                "www.spanx.com": {
-                    WBRS_SCORE: '1.58',
-                    WBRS_Rule_Hits: "alx_cln, vsvd",
-                    Hostname_ips: '',
-                    current_cat: "Shopping",
-                    cat_sugg: [
+    ActionController::Parameters.new({
+        'source_key' => 49,
+        'source_type' => "Complaint",
+        'payload' => {
+            'name' => "Marlin Pierce",
+            'email' => "marlpier@cisco.com",
+            'domain' => "cisco.com",
+            'problem' => "New category",
+            'details' => '',
+            'user_ip' => "::1",
+            'ticket_time' => "September 03, 2018 12:05",
+            'investigate_ips' => {},
+            'investigate_urls' => {
+                'www.spanx.com' => {
+                    'WBRS_SCORE' => '1.58',
+                    'WBRS_Rule_Hits' => "alx_cln, vsvd",
+                    'Hostname_ips' => '',
+                    'current_cat' => "Shopping",
+                    'cat_sugg' => [
                         "Lingerie and Swimsuits"
                     ],
                 }
             },
-            user_company: "Cisco Systems, Inc.",
-            submission_type: "w"
+            'user_company' => "Cisco Systems, Inc.",
+            'submission_type' => "w"
         }
-    }
+    })
   end
 
   before(:example) do
