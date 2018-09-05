efe66b7598b8f06a546beeef50966a9b9c5f2fbd
Marlin W. Pierce <marlpier@cisco.com>
2018-09-05 15:59:29 -0400

    Initial complaint message test.

---
+++ spec/models/complaint_spec.rb
@@ -0,0 +1,41 @@
+describe Complaint do
+  let(:complaint_message_payload) do
+    {
+        source_key: 49,
+        source_type: "Complaint",
+        payload: {
+            name: "Marlin Pierce",
+            email: "marlpier@cisco.com",
+            domain: "cisco.com",
+            problem: "New category",
+            details: '',
+            user_ip: "::1",
+            ticket_time: "September 03, 2018 12:05",
+            investigate_ips: {},
+            investigate_urls: {
+                "www.spanx.com": {
+                    WBRS_SCORE: '1.58',
+                    WBRS_Rule_Hits: "alx_cln, vsvd",
+                    Hostname_ips: '',
+                    current_cat: "Shopping",
+                    cat_sugg: [
+                        "Lingerie and Swimsuits"
+                    ],
+                }
+            },
+            user_company: "Cisco Systems, Inc.",
+            submission_type: "w"
+        }
+    }
+  end
+
+  before(:example) do
+    FactoryBot.create(:vrt_incoming_user)
+    FactoryBot.create(:guest_company)
+  end
+
+  it 'processes bridge payload' do
+
+    Complaint.process_bridge_payload(complaint_message_payload)
+  end
+end
--- spec/requests/peake_bridge/poll_from_bridge_request_spec.rb
+++ spec/requests/peake_bridge/poll_from_bridge_request_spec.rb
@@ -1,13 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Talos Intelligence poll-from-bridge channel", type: :request do
-  it 'receives unknown message' do
-
-    post '/escalations/peake_bridge/channels/unknown/messages'
-
-    expect(response.code).to eq('400')
-  end
-
   let(:dispute_email) do
     <<-HEREDOC
         ____________________________________________________________
