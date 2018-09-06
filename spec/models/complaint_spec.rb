93ad403aeda992361fb3777a5936131c8f0c1a9f
Marlin W. Pierce <marlpier@cisco.com>
2018-09-06 15:54:40 -0400

    api stub returns results and stubs history.

--- spec/models/complaint_spec.rb
+++ spec/models/complaint_spec.rb
@@ -30,12 +30,70 @@ describe Complaint do
   end
   let(:rules_get_json) do
     {
-        "data" => [],
+        "data" => [
+            {
+                "category" => 5,
+                "desc_long" => "Auctions; bartering; online purchasing; coupons and free offers; yellow pages; classified ads; general office supplies; online catalogs; online malls.",
+                "descr" => "Shopping",
+                "mnem" => "shop",
+                "domain" => "spanx.com",
+                "is_active" => 1,
+                "path" => "",
+                "path_hashed" => "",
+                "port" => 0,
+                "prefix_id" => 2,
+                "protocol" => "http",
+                "subdomain" => "",
+                "truncated" => 0
+            }
+        ],
         "errors" => [],
         "meta" => {
             "limit" => 1000,
-            "rows_found" => 0
+            "rows_found" => 1
         }
+    }.to_json
+  end
+  let(:audit_json) do
+    {
+        "data" => [
+            {
+                "action" => "insert",
+                "category_id" => 5,
+                "confidence" => 1,
+                "description" => "",
+                "event_id" => 1,
+                "prefix_id" => 1,
+                "time" => "Tue, 20 Mar 2018 14:44:05 GMT",
+                "user" => "tester333"
+            },
+            {
+                "action" => "update",
+                "category_id" => 5,
+                "confidence" => 1,
+                "description" => "",
+                "event_id" => 2,
+                "prefix_id" => 1,
+                "time" => "Tue, 20 Mar 2018 14:56:28 GMT",
+                "user" => "tester"
+            }
+        ],
+        "meta" => {
+            "limit" => 1000,
+            "rows_found" => 2
+        }
+    }.to_json
+  end
+  let(:categories_json) do
+    {
+        "data": [
+            {
+                "category": 5,
+                "desc_long": "Auctions; bartering; online purchasing; coupons and free offers; yellow pages; classified ads; general office supplies; online catalogs; online malls.",
+                "descr": "Shopping",
+                "mnem": "shop"
+            }
+        ]
     }.to_json
   end
   let(:top_url_response_json) do
@@ -44,6 +102,8 @@ describe Complaint do
     }.to_json
   end
   let(:rules_get_response) { double('HTTPI::Response', code: 200, body: rules_get_json) }
+  let(:audit_response) { double('HTTPI::Response', code: 200, body: audit_json) }
+  let(:categories_response) { double('HTTPI::Response', code: 200, body: categories_json) }
   let(:top_url_response_response) { double('HTTPI::Response', code: 200, body: top_url_response_json) }
   let(:bug_factory) do
     double('Bugzilla::Bug', create: { "id" => 101 })
@@ -62,6 +122,14 @@ describe Complaint do
         .to receive(:post_request)
                 .with(path: '/v1/cat/rules/get', body: anything)
                 .and_return(rules_get_response)
+    allow(Wbrs::Base)
+        .to receive(:post_request)
+                .with(path: '/v1/cat/rules/audit', body: anything)
+                .and_return(audit_response)
+    allow(Wbrs::Base)
+        .to receive(:call_json_request)
+                .with(:get, '/v1/cat/categories', body: anything)
+                .and_return(categories_response)
     allow(Wbrs::Base)
         .to receive(:call_json_request)
                 .with(:post, '/v1/cat/urls/top', body: anything)
