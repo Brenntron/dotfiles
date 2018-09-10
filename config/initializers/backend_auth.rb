9c816cb654925a0bf322fc08d16907707bc2cb92
Marlin W. Pierce <marlpier@cisco.com>
2018-09-10 12:34:02 -0400

    use nil for all backend_auth.

--- config/initializers/backend_auth.rb
+++ config/initializers/backend_auth.rb
@@ -13,4 +13,6 @@ if Rails.env.development? || Rails.env.test?
       authenticate_cec_username: ENV['authenticate_cec_username'],
       authenticate_display_name: ENV['authenticate_display_name']
   }
+else
+  Rails.configuration.backend_auth = {}
 end
