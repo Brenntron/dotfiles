cb19f3d54cc4bd2317cbf18d38ff5249fe2512ef
Marlin W. Pierce <marlpier@cisco.com>
2018-10-09 10:55:35 -0400

    symbol + colon hash syntax

--- spec/requests/escalations/webrep/root_request_spec.rb
+++ spec/requests/escalations/webrep/root_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 require 'authentication_helper'
 
-RSpec.describe Escalations::Webrep::RootController, :type => :request do
+RSpec.describe Escalations::Webrep::RootController, type: :request do
   it 'redirects index routes to complaints index' do
     create_login_session(roles: ['webcat user'])
 
--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 # require 'authentication_helper'
 
-RSpec.describe SessionsController, :type => :request do
+RSpec.describe SessionsController, type: :request do
   it 'redirects to session new when not logged in' do
 
     get escalations_root_path
