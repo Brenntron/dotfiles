1afb180d7d0967a5a8122794dea5afa1cc3a06fb
Chris LaClair <claclair@cisco.com>
2018-10-19 12:45:41 -0400

    #300: server side data for front end chart

--- app/controllers/api/v1/escalations/webrep/reports.rb
+++ app/controllers/api/v1/escalations/webrep/reports.rb
@@ -80,6 +80,25 @@ module API
               response_data.to_json
             end
 
+            params do
+              requires :from, type: String
+              requires :to, type: String
+              requires :user_id, type: Integer, desc: ""
+              requires :submitter_types, type: Array[String]
+              requires :submission_types, type: Array[String]
+            end
+
+            get 'closed_ticket_entries_by_resolution_report' do
+              authorize!(:index, Dispute)
+
+              report_data = Dispute.closed_ticket_entries_by_resolution_report(params[:user_id], params[:from], params[:to], params[:submission_types], params[:submitter_types])
+
+              response_data = {:status => "success", :data => report_data}
+
+              response_data.to_json
+            end
+
+
           end
         end
       end
