af119f1b7b58d44593fb9e33ef11c63a31214dba
NickHerbert <nherbert@sourcefire.com>
2018-11-08 16:54:14 -0500

    fixes #377 initial AMP FP model

---
+++ app/models/amp_false_positive.rb
@@ -0,0 +1,22 @@
+class AmpFalsePositive < ApplicationRecord
+  has_paper_trail on: [:update], ignore: [:updated_at]
+  belongs_to :user, :optional => true
+  belongs_to :customer
+
+  AC_SUCCESS = 'CREATE_ACK'
+  AC_FAILED = 'CREATE_FAILED'
+  AC_PENDING = 'CREATE_PENDING'
+
+  TI_NEW = 'PENDING'
+  TI_RESOLVED = 'RESOLVED'
+  TI_CLOSED = 'CLOSED'
+
+  def self.process_bridge_payload(message_payload)
+    begin
+      ActiveRecord::Base.transaction do
+
+      end
+    end
+  end
+
+end
