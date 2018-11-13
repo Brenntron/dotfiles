efccf6aed3b5acd943bd571ea8959f4805eccc09
Andrew Cheng <ancheng3@cisco.com>
2018-11-13 13:56:23 -0500

    Set Status Comment Button to display either 'resolution_comment' or 'status_comment' & Wired functionality to set a dispute to have either status_comment or resolution_comment -- but not both

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -934,8 +934,10 @@ class Dispute < ApplicationRecord
         dispute.resolution_comment = comment
         dispute.case_closed_at = resolved_at
         dispute.case_resolved_at = resolved_at
+        dispute.status_comment = nil
       else
         dispute.resolution = nil
+        dispute.resolution_comment = nil
         dispute.status_comment = comment
       end
 
@@ -1014,7 +1016,13 @@ class Dispute < ApplicationRecord
       dispute_packet = dispute.attributes.slice(*%w{id priority status resolution})
       dispute_packet[:case_number] = dispute.case_id_str
       dispute_packet[:status] = "<span class='dispute_status' id='status_#{dispute.id}'> #{dispute.status} </span>"
-      dispute_packet[:status_comment] = dispute&.status_comment
+      dispute_packet[:status_comment] = nil
+      if dispute&.status_comment.present?
+        dispute_packet[:status_comment] = dispute&.status_comment
+      end
+      if dispute&.resolution_comment.present?
+        dispute_packet[:status_comment] = dispute&.resolution_comment
+      end
       dispute_packet[:case_link] = "<a href='/escalations/webrep/disputes/#{dispute.id}'>" + dispute_packet[:case_number] + "</a>"
       dispute_packet[:submitter_org] = dispute.customer.company.name
       dispute_packet[:submitter_type] = dispute.submitter_type
