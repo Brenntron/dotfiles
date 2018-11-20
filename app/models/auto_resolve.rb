31d1e5fd8013630e100b0417806ee55467ef4d12
marlpier <marlpier@cisco.com>
2018-11-20 09:08:08 -0500

    Update auto_resolve.rb

--- app/models/auto_resolve.rb
+++ app/models/auto_resolve.rb
@@ -98,7 +98,7 @@ class AutoResolve
   end
 
   def check_virus_total_from_preload(dispute_entry, address)
-    if dispute_entry.present? && dispute_entry.dispute_entry_preload.present? && dispute_entry.dispute_entry_preload.virustotal.present?
+    if dispute_entry&.dispute_entry_preload&.virustotal.present?
       result = JSON.parse(dispute_entry.dispute_entry_preload.virustotal)
     else
       result = Virustotal::Scan.scan_hashes(address: address)
