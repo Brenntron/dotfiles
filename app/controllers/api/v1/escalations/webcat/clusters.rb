fea34fa9592409e3fa8214c3103a0574c02657cf
Chris LaClair <claclair@cisco.com>
2018-12-05 10:51:35 -0500

    #15: fixed papertrail

--- app/controllers/api/v1/escalations/webcat/clusters.rb
+++ app/controllers/api/v1/escalations/webcat/clusters.rb
@@ -8,7 +8,7 @@ module API
           resource "escalations/webcat/clusters" do
 
             before do
-              PaperTrail.whodunnit = current_user.id if current_user.present?
+              PaperTrail.request.whodunnit = current_user.id if current_user.present?
             end
 
             desc 'process cluster'
