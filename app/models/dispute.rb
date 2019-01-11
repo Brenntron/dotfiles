614d5ec4df1858f83364905d0275eaa9457cbbd8
Andrew Cheng <ancheng3@cisco.com>
2019-01-11 14:09:39 -0500

    Added safe navigator to setting a new dispute's customer_id value

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -428,7 +428,7 @@ class Dispute < ApplicationRecord
         new_dispute.submission_type = message_payload["payload"]["submission_type"]  # email, web, both  [e|w|ew]
         new_dispute.status = NEW
 
-        new_dispute.customer_id = customer.id
+        new_dispute.customer_id = customer&.id
 
         new_dispute.submitter_type = new_dispute.customer.company_id == guest.id ? SUBMITTER_TYPE_NONCUSTOMER : SUBMITTER_TYPE_CUSTOMER
 
