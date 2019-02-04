a5ce216eb28d77898f7b86a46cbfd29359e034f7
micah <mimcfarl@cisco.com>
2019-02-04 10:26:18 -0500

    Removes redundant migrations

--- db/migrate/20190129155915_increase_url_primary_category_size_in_complaint_entries.rb
+++
@@ -1,5 +0,0 @@
-class IncreaseUrlPrimaryCategorySizeInComplaintEntries < ActiveRecord::Migration[5.2]
-  def change
-    change_column :complaint_entries, :url_primary_category, :string, :limit => 1000
-  end
-end
--- db/migrate/20190129160213_increase_category_size_in_complaint_entries.rb
+++
@@ -1,5 +0,0 @@
-class IncreaseCategorySizeInComplaintEntries < ActiveRecord::Migration[5.2]
-  def change
-    change_column :complaint_entries, :category, :string, :limit => 1000
-  end
-end
