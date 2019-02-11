ec2ebb9f5b75f933fdd61836fef39a547dc90d0e
nherbert <nherbert@sourcefire.com>
2019-02-11 10:42:43 -0500

    Update 20190205164048_fix_auto_resolve_entries.sql

--- db/sql/20190205164048_fix_auto_resolve_entries.sql
+++ db/sql/20190205164048_fix_auto_resolve_entries.sql
@@ -3,3 +3,4 @@ INNER JOIN `disputes` ON `disputes`.`id` = `dispute_entries`.`dispute_id`
 SET `dispute_entries`.`status` = "RESOLVED_CLOSED"
 WHERE
 `disputes`.`resolution` = 'All Auto Resolved'
+INSERT INTO `schema_migrations` (`version`) VALUES ('20190205164048');
