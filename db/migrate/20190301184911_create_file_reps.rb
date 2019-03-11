83848f6fbc1ec40edf655061b0e9990b62539268
Marlin W. Pierce <marlpier@cisco.com>
2019-03-11 16:25:33 -0400

    Add `file_rep_name` field.

--- db/migrate/20190301184911_create_file_reps.rb
+++ db/migrate/20190301184911_create_file_reps.rb
@@ -2,6 +2,7 @@ class CreateFileReps < ActiveRecord::Migration[5.2]
   def change
     create_table :file_reps do |t|
       t.timestamps
+      t.string :file_rep_name
       t.text :sha256
       t.string :email
     end
