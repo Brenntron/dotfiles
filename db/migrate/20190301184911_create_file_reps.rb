85152f9eb391fded849a04944d3bc0394aa0e41b
Marlin W. Pierce <marlpier@cisco.com>
2019-03-14 14:35:34 -0400

    Rename sha256 to sha256_checksum

--- db/migrate/20190301184911_create_file_reps.rb
+++ db/migrate/20190301184911_create_file_reps.rb
@@ -3,7 +3,7 @@ class CreateFileReps < ActiveRecord::Migration[5.2]
     create_table :file_reps do |t|
       t.timestamps
       t.string :file_rep_name
-      t.text :sha256
+      t.text :sha256_checksum
       t.string :email
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -529,7 +529,7 @@ ActiveRecord::Schema.define(version: 2019_03_01_184911) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "file_rep_name"
-    t.text "sha256"
+    t.text "sha256_checksum"
     t.string "email"
   end
 
