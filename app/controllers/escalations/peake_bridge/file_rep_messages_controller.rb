6b7481a3e2c883e1f0d52b599cf59988a16ec7f7
Marlin W. Pierce <marlpier@cisco.com>
2019-03-20 13:36:44 -0400

    Updated file_rep_params

--- app/controllers/escalations/peake_bridge/file_rep_messages_controller.rb
+++ app/controllers/escalations/peake_bridge/file_rep_messages_controller.rb
@@ -7,13 +7,17 @@ class Escalations::PeakeBridge::FileRepMessagesController < ApplicationControlle
       render plain: 'successfully created file rep', status: :ok
     else
       error_messages = file_rep.errors.full_messages.join('; ')
-      render plain: "Error(s) creating file rep -- #{error_messages}", status: :ok
+      render plain: "Error(s) creating file rep -- #{error_messages}", status: :internal_server_error
     end
   end
 
   private
 
+  def sender_params
+    params.require(:message).require(:sender_data).permit(:ticketable_type, :ticketable_id, :message_id)
+  end
+
   def file_rep_params
-    params.require(:message).permit(:file_rep_name, :sha256_checksum, :email)
+    params.require(:message).require(:file_rep).permit(:file_rep_name, :sha256_checksum, :email)
   end
 end
