102bfe87897013a55a52ea0d7b1e0045f1eaa9d9
Katherine Hill <kathehil@Katherines-MacBook-Pro.local>
2019-04-01 08:49:59 -0400

    adding new ui quicklookup elemetns

--- app/assets/javascripts/admin_redesign.js
+++ app/assets/javascripts/admin_redesign.js
@@ -38,6 +38,7 @@
 //= require dataTables/extras/dataTables.select
 //= require application/webcat/complaints
 //= require application/webcat/clusters
+//= require application/webcat/research_lookup
 //= require application/webcat/complaint_form
 //= require application/webrep/dispute_form
 //= require application/webrep_research_tab
