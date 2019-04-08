6443d1f0a0b7a56939d836608641084f44a43d12
Marlin W. Pierce <marlpier@cisco.com>
2019-04-08 16:32:13 -0400

    FactoryBot file reputation factory.

--- app/models/file_reputation_dispute.rb
+++ app/models/file_reputation_dispute.rb
@@ -3,4 +3,12 @@ class FileReputationDispute < ApplicationRecord
   belongs_to :customer, optional:true
   delegate :name, :company, :company_id, to: :customer, allow_nil: true, prefix: true
 
+  STATUS_NEW                = 'NEW'
+  STATUS_ASSIGNED           = 'ASSIGNED'
+  STATUS_CLOSED             = 'CLOSED'
+
+  DISPOSITION_MALICIOUS     = 'MALICIOUS'
+
+  validates :status, :file_name, :sha256_hash, :disposition_suggested, presence: true
+
 end
