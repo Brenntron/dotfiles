e91f5168c07402dda148c40a6ed00335b7a72715
Andrew Cheng <ancheng3@cisco.com>
2019-04-10 17:14:11 -0400

    WEB-4402, Added ThreatGrid & SandBox API Key Fields To User Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -158,7 +158,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:parent_id, :display_name,:cvs_username, :cec_username, :kerberos_login, :committer, :confirmed, :email, :class_level, :metrics_timeframe, role_ids: [])
+    params.require(:user).permit(:parent_id, :display_name,:cvs_username, :cec_username, :kerberos_login, :committer, :confirmed, :email, :class_level, :metrics_timeframe,  :threatgrid_api_key, :sandbox_api_key, role_ids: [])
   end
 
   def user_api_key_params
