0e2ae0d4996ab57e2d0139538fcc69a699349847
micah <mimcfarl@cisco.com>
2019-04-10 13:06:45 -0400

    we were given the wrong ReversingLabs URL but saving anyway to come back to later

--- app/models/file_reputation_api/reversing_labs.rb
+++ app/models/file_reputation_api/reversing_labs.rb
@@ -1,8 +1,9 @@
-class ReversingLabsApi < ApplicationRecord
-  include ApiRequester::ApiRequester
+class FileReputationApi::ReversingLabs
 
   # TODO: Either open a ticket to make ApiRequester support HTTP basic auth OR come back here and
   # set up this stuff when it does.
+
+  #       include ApiRequester::ApiRequester
 
   #       set_api_requester_config Rails.configuration.reversing_labs
   #       set_default_request_type :json
@@ -21,21 +22,25 @@ class ReversingLabsApi < ApplicationRecord
   end
 
   def self.get_reversing_labs_response(uri)
-    request = Net::HTTP::Get.new(uri)
+    request = Net::HTTP::Post.new(uri)
+    binding.pry
     request.basic_auth reversing_labs_username, reversing_labs_password
     req_options = {
         use_ssl: uri.scheme == "https",
         verify_mode: OpenSSL::SSL::VERIFY_NONE
     }
 
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    response = Net::HTTP.start(uri.hostname, 443, req_options) do |http|
       http.request(request)
     end
+
+    binding.pry
 
     response
   end
 
   def self.handle_reversing_labs_response(response)
+    # binding.pry
     if response.code != "200" #there was an issue
       {error: 'Data Currently Unavailable'}
     else
@@ -48,19 +53,19 @@ class ReversingLabsApi < ApplicationRecord
   end
 
   def self.sample_upload(sha1_value)
-    uri = URI(reversing_labs_url + "/api/spex/upload/#{sha1_value}")
+    uri = URI(reversing_labs_url + "api/spex/upload/#{sha1_value}")
     response = get_reversing_labs_response(uri)
     handle_reversing_labs_response(response)
   end
 
   def self.sample_upload_status(sha1_value)
-    uri = URI(reversing_labs_url + "/api/spex/upload/#{sha1_value}/status")
+    uri = URI(reversing_labs_url + "api/spex/upload/#{sha1_value}/status")
     response = get_reversing_labs_response(uri)
     handle_reversing_labs_response(response)
   end
 
   def self.sample_download(sha1_value)
-    uri = URI(reversing_labs_url + "/api/spex/download/#{sha1_value}")
+    uri = URI(reversing_labs_url + "api/spex/download/#{sha1_value}")
     response = get_reversing_labs_response(uri)
     handle_reversing_labs_response(response)
   end
