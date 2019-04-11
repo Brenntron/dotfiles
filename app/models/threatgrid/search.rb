db8bbc4233523b0230c3e1d2d7256d4816701fbc
Andrew Cheng <ancheng3@cisco.com>
2019-04-11 18:19:44 -0400

    Added placeholders for 'data' method to fetch specific fields from ThreatGrid API

--- app/models/threatgrid/search.rb
+++ app/models/threatgrid/search.rb
@@ -15,8 +15,29 @@ class Threatgrid::Search
 
   def self.data(sha256_hash)
     api_response = call_request_parsed(:get, '/api/v2/search/submissions', input: {q: sha256_hash, sort_by: 'timestamp', api_key: "#{Rails.configuration.threatgrid.api_key}"})
+
+    submitted_to_tg = api_response
+    run_status = api_response
     threat_score = api_response&.dig('data','items')[0]&.dig('item','analysis','threat_score')
 
-    {threat_score: threat_score}
+    # Tags (can contain many entries)
+    tag_data = api_response
+    tags = api_response
+
+    control = api_response
+    vm_name = api_response
+    run_time = api_response
+    os = api_response
+
+    # Behaviors (can contain many entries)
+    behavior_data = api_response
+    behaviors = api_response
+
+    full_json = api_response.to_json
+
+
+
+    {submitted_to_tg: submitted_to_tg, run_status: run_status, threat_score: threat_score, tags: tags, control: control,
+     vm_name: vm_name, run_time: run_time, os: os, behaviors: behaviors, full_json: full_json}
   end
 end
