841162aeaf3e003da0523f0202de8d447b5f8c98
Andrew Cheng <ancheng3@cisco.com>
2019-04-11 13:06:45 -0400

    Set 'file_reputation_dispute_id' to be an integer.

--- db/migrate/20190411153233_file_rep_comments.rb
+++ db/migrate/20190411153233_file_rep_comments.rb
@@ -2,7 +2,7 @@ class FileRepComments < ActiveRecord::Migration[5.2]
   def change
     create_table :file_rep_comments do |t|
       t.timestamps
-      t.string :file_reputation_dispute_id
+      t.integer :file_reputation_dispute_id
       t.text :comment
       t.integer :user_id
     end
