618a01ad42753868d9234d1063d304021dcde47a
Marlin W. Pierce <marlpier@cisco.com>
2019-04-16 11:06:26 -0400

    standardized config

--- config/initializers/config_yml.rb
+++ config/initializers/config_yml.rb
@@ -142,12 +142,7 @@ Rails.configuration.bls.port           = bls_config['port']
 
 file_reputation_sandbox = env_config['file_reputation_sandbox']
 raise 'config.yml missing file reputation sandbox section' unless file_reputation_sandbox
-Rails.configuration.file_reputation_sandbox        = OpenStruct.new
-Rails.configuration.file_reputation_sandbox.host   = file_reputation_sandbox['host']
-Rails.configuration.file_reputation_sandbox.port   = file_reputation_sandbox['port']
-Rails.configuration.file_reputation_sandbox.verify_mode   = file_reputation_sandbox['verify_mode']
-Rails.configuration.file_reputation_sandbox.tls   = file_reputation_sandbox['tls']
-Rails.configuration.file_reputation_sandbox.api_key   = file_reputation_sandbox['api_key']
+Rails.configuration.file_reputation_sandbox        = ApiRequester::ApiRequester.config_of(file_reputation_sandbox)
 
 reversing_labs_config = env_config['reversing_labs']
 raise 'config.yml missing ReversingLabs section' unless reversing_labs_config
