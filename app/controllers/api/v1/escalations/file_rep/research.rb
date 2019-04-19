c02bb7a6dcb501d73975fe2bb6633b4ae29c69cb
Marlin W. Pierce <marlpier@cisco.com>
2019-04-19 14:06:01 -0400

    Error handling

--- app/controllers/api/v1/escalations/file_rep/research.rb
+++ app/controllers/api/v1/escalations/file_rep/research.rb
@@ -10,11 +10,18 @@ module API
               requires :sha256_hash, type: String, desc: "SHA256 hash"
             end
             post "" do
-              sha256_hash = permitted_params[:sha256_hash]
-              api_response = Threatgrid::Search.data(sha256_hash)
-              attributes = Threatgrid::Search.query_from_data(api_response)
-              FileReputationDispute.where(sha256_hash: sha256_hash).update_all(attributes)
-              render json: api_response
+              std_api_v2 do
+                if /\A\H*(?<sha256_hash>\h{64})\H*\z/ =~ permitted_params[:sha256_hash]
+                  api_response = Threatgrid::Search.data(sha256_hash)
+                  attributes = Threatgrid::Search.query_from_data(api_response)
+                  FileReputationDispute.where(sha256_hash: sha256_hash).update_all(attributes)
+                  render json: api_response
+                else
+                  exception = RuntimeError.new('Not a valid SHA256')
+                  exception.set_backtrace(caller)
+                  std_exception(exception, status: :bad_request)
+                end
+              end
             end
           end
         end
