cc21728360bf71d4aa09b71da413c5b24bb2064d
Marlin W. Pierce <marlpier@cisco.com>
2019-05-10 14:11:11 -0400

    Implement constructors.

--- app/models/file_reputation_api/reversing_labs.rb
+++ app/models/file_reputation_api/reversing_labs.rb
@@ -3,10 +3,25 @@
 # 99e432ac19e5a47d0d1ddfad9f326d5e169ab6651d844d4b800a79f4f78d410f
 
 class FileReputationApi::ReversingLabs
+  include ActiveModel::Model
+  attr_accessor :sha256_hash, :raw_json
+
   include ApiRequester::ApiRequester
   set_api_requester_config Rails.configuration.reversing_labs
   set_default_request_type :query_string
   set_basic_auth
+
+  def api_response
+    @api_response ||= JSON.parse!(self.raw_json)
+  end
+
+  def self.cache_key(sha256_hash)
+    "reversing_labs:#{sha256_hash}"
+  end
+
+  def cache_key
+    @cache_key ||= self.class.cache_key(self.sha256_hash)
+  end
 
   def self.sha256_lookup(sha256)
     cache_key = "reversing_labs:#{sha256}"
@@ -42,6 +57,21 @@ class FileReputationApi::ReversingLabs
     FileReputationApi::ReversingLabs.score_of_lookup(api_response)
   end
 
+  def score
+    reversing_labs_score = 0
+    reversing_labs_count = 0
+    if api_response&.dig('rl','sample','xref','entries')&.any?
+      api_response&.dig('rl','sample','xref','entries')[0]&.dig('scanners').each do |scanner|
+        reversing_labs_count += 1
+        if scanner['result'].present?
+          reversing_labs_score += 1
+        end
+      end
+    end
+
+    { reversing_labs_score: reversing_labs_score, reversing_labs_count: reversing_labs_count }
+  end
+
   # Returns an array of certificates
   def self.certificates(sha256)
     api_response = call_request_parsed(:post, '/api/databrowser/rldata/bulk_query/json', request_type: :json, input: {rl: {query: {hash_type: 'sha256', hashes: [sha256] }}}, headers: {'Authorization': 'Basic dS9zb3VyY2VmaXJlOlV1djRsYWl0'})
@@ -53,5 +83,46 @@ class FileReputationApi::ReversingLabs
     end
 
     certificates
+  end
+
+  # Makes an immediate direct call to reversing labs and creates an object.
+  # Does not read or write cache or database.
+  # @param [String] sah256_hash the SHA256 hash checksum.
+  # @return [FileReputationApi::ReversingLabs] object for the result.
+  def self.lookup_raw(sha256_hash)
+    response = call_request(:get, "api/databrowser/rldata/query/sha256/#{sha256_hash}", input: {format: 'json'})
+
+    # raise exception if JSON is invalid
+    JSON.parse!(response.body)
+
+    new(sha256_hash: sha256_hash, raw_json: response.body)
+  end
+
+  # Makes an immediate call to get up to date reversing labs results and stores data in cache and database.
+  # @param [String] sah256_hash the SHA256 hash checksum.
+  # @return [FileReputationApi::ReversingLabs] object for the result.
+  def self.lookup_immediate(sha256_hash)
+    rev_labs = lookup_raw(sha256_hash)
+
+    Rails.cache.write(rev_labs.cache_key, rev_labs.raw_json)
+
+    score_attributes = rev_labs.score
+    attributes = score_attributes.merge(reversing_labs_raw: rev_labs.raw_json)
+    FileReputationDispute.where(sha256_hash: sha256_hash).update_all(attributes)
+
+    rev_labs
+  end
+
+  # Gets the reversing labs data.  May read cached data, so might not be immediate.
+  # @param [String] sah256_hash the SHA256 hash checksum.
+  # @return [FileReputationApi::ReversingLabs] object for the result.
+  def self.lookup(sha256_hash)
+    cached_json = Rails.cache.read(cache_key(sha256_hash))
+
+    if cached_json.blank?
+      lookup_immediate(sha256_hash)
+    else
+      new(sha256_hash: sha256_hash, raw_json: cached_json)
+    end
   end
 end
