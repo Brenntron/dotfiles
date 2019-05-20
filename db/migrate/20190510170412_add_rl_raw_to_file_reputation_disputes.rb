5e8f22c6c958a017f6f7aec6e7cb7aff5dc6e9ab
Marlin W. Pierce <marlpier@cisco.com>
2019-05-20 11:37:42 -0400

    Changed RL raw field to mediumtext

--- db/migrate/20190510170412_add_rl_raw_to_file_reputation_disputes.rb
+++ db/migrate/20190510170412_add_rl_raw_to_file_reputation_disputes.rb
@@ -1,5 +1,5 @@
 class AddRlRawToFileReputationDisputes < ActiveRecord::Migration[5.2]
   def change
-    add_column :file_reputation_disputes, :reversing_labs_raw, :text
+    add_column :file_reputation_disputes, :reversing_labs_raw, :mediumtext
   end
 end
--- db/sql/20190510170412_add_rl_raw_to_file_reputation_disputes.sql
+++ db/sql/20190510170412_add_rl_raw_to_file_reputation_disputes.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `file_reputation_disputes` ADD `reversing_labs_raw` text;
+ALTER TABLE `file_reputation_disputes` ADD `reversing_labs_raw` mediumtext;
 INSERT INTO `schema_migrations` (`version`) VALUES ('20190510170412');
