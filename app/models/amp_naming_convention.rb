9cc082023997d5e1c5faf0657d6c70c78b7e736b
Marlin W. Pierce <marlpier@cisco.com>
2019-05-23 10:55:48 -0400

    Implement update from params in AMP naming convention ActiveRecord class.

--- app/models/amp_naming_convention.rb
+++ app/models/amp_naming_convention.rb
@@ -2,4 +2,16 @@ class AmpNamingConvention < ApplicationRecord
   validates :pattern, :example, :engine_description, :public_notes, presence: true
   validates :table_sequence, presence: true, numericality: { only_integer: true }
 
+  def update_from_params(params)
+    attribute_values = params.slice(*AmpNamingConvention.column_names)
+    attribute_values.delete('id')
+
+    ti_pattern = TiApi::AmpNamingPattern.new(self) #sets the old table_sequence
+    assign_attributes(attribute_values)
+    raise errors.full_messages.to_sentence unless valid?
+    ti_pattern.update!(self)
+    save!
+
+    return true
+  end
 end
