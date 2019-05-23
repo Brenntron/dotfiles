36cb6890e59e4e30eeb55823fe2d2ad38d9001b3
Marlin W. Pierce <marlpier@cisco.com>
2019-05-23 10:55:08 -0400

    Complete implementation on update from params.

--- app/models/ti_api/amp_naming_pattern.rb
+++ app/models/ti_api/amp_naming_pattern.rb
@@ -1,18 +1,18 @@
 class TiApi::AmpNamingPattern < TiApi::Base
-  attr_reader :m_amp_naming_convention, :old_position
+  attr_reader :m_amp_naming_convention, :prev_position
 
   def initialize(m_amp_naming_convention)
     @m_amp_naming_convention = m_amp_naming_convention
-    @old_position = m_amp_naming_convention.table_sequence
+    @prev_position = m_amp_naming_convention.table_sequence
   end
 
-  def update(amp_naming_convention_given = m_amp_naming_convention)
+  def update!(amp_naming_convention_given = m_amp_naming_convention)
     return false unless amp_naming_convention_given.valid?
     input = {
         ticode: Rails.configuration.talos_intelligence.api_key,
         amp_naming_pattern: {
-            old_position: old_position,
-            new_position: amp_naming_convention_given.table_sequence,
+            old_position: prev_position,
+            position: amp_naming_convention_given.table_sequence,
             pattern: amp_naming_convention_given.pattern,
             example: amp_naming_convention_given.example,
             description: amp_naming_convention_given.engine_description,
@@ -20,6 +20,7 @@ class TiApi::AmpNamingPattern < TiApi::Base
         }
     }
     self.class.call_request(:put, 'api/v1/amp_naming_patterns', input: input)
+    @prev_position = amp_naming_convention_given.table_sequence
     return true
   end
 end
