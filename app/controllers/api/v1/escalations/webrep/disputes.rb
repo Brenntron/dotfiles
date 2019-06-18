51c2308e52a16244113313a1423393ad7c871c91
micah <mimcfarl@cisco.com>
2019-06-18 08:56:25 -0400

    Removes classification strings that the rep tool doesn't like

--- app/controllers/api/v1/escalations/webrep/disputes.rb
+++ app/controllers/api/v1/escalations/webrep/disputes.rb
@@ -300,6 +300,7 @@ module API
             end
             post "reptool_bl" do
               std_api_v2 do
+                params["classifications"] = params["classifications"].slice! "No active classifications,"
                 RepApi::Blacklist.adjust_from_params(permitted_params, username: current_user.cvs_username)
                 true
               end
@@ -312,6 +313,7 @@ module API
             post "maintain_reptool_bl" do
               std_api_v2 do
                 permitted_params['data'].each do |entry|
+                  entry["classifications"] = entry["classifications"].slice! "No active classifications,"
                   RepApi::Blacklist.adjust_from_params(entry, username: current_user.cvs_username)
                 end
                 true
@@ -325,6 +327,7 @@ module API
             end
             post "drop_reptool_bl" do
               std_api_v2 do
+                entry["classifications"] = entry["classifications"].slice! "No active classifications,"
                 RepApi::Blacklist.adjust_from_params(permitted_params, username: current_user.cvs_username)
                 true
               end
