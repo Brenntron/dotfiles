25357971f79447a9e79e5ef6ea2280983703dbd4
Chris LaClair <claclair@cisco.com>
2019-06-27 09:07:03 -0400

    WEB-4284: cleanup

--- app/controllers/escalations/other_admin_tools/tools_controller.rb
+++ app/controllers/escalations/other_admin_tools/tools_controller.rb
@@ -9,11 +9,5 @@ class Escalations::OtherAdminTools::ToolsController < ApplicationController
     @available_tasks = AdminTask.available_tasks 
   end
 
-  def execute_tasks
-    task_name = params[:name]
-    task_arguments = params[:task_arguments]
-
-    AdminTask.process_task(task_name, task_arguments)
-  end  
 end
 
