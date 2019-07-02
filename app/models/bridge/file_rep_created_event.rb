9f82f3905585e619da06bc40a4ac57cad3985ec2
Andrew Cheng <ancheng3@cisco.com>
2019-07-02 14:12:46 -0400

    Pass through 'ac_id' on FileRepCreatedEvent

--- app/models/bridge/file_rep_created_event.rb
+++ app/models/bridge/file_rep_created_event.rb
@@ -2,12 +2,14 @@ class Bridge::FileRepCreatedEvent < Bridge::BaseMessage
   def initialize(addressee:, source_authority: nil, source_key: nil, ac_id:)
     super(channel: 'ticket-acknowledge',
           addressee: addressee)
+    @ac_id = ac_id
     @source_authority = source_authority
     @source_key = source_key
   end
 
-  def post(payload, source_authority: @source_authority, source_key: @source_key)
-    super(message: {source_authority: source_authority,
+  def post(payload, source_authority: @source_authority, source_key: @source_key, ac_id: @ac_id)
+    super(message: {ac_id: ac_id,
+                    source_authority: source_authority,
                     source_key: source_key,
                     ticket_entries: payload,
                     ac_status: FileReputationDispute::AC_SUCCESS
