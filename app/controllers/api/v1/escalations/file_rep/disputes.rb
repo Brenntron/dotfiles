68a4f38935b92db76809c7969d1113c336fcbcfe
Andrew Cheng <ancheng3@cisco.com>
2019-07-08 21:24:06 -0400

    Optimized code

--- app/controllers/api/v1/escalations/file_rep/disputes.rb
+++ app/controllers/api/v1/escalations/file_rep/disputes.rb
@@ -45,7 +45,6 @@ module API
                 duplicates = []
                 uniques = {}
                 user_validation = User.where(cvs_username: params[:assignee])
-                assignee_id = User.where(cvs_username: params[:assignee]).first.id
 
                 if user_validation.exists?
                   permitted_params['shas_array'].each do |sha256|
@@ -55,7 +54,7 @@ module API
                       file_rep = FileReputationDispute.create_through_form(bugzilla_rest_session,
                                                                 sha256,
                                                                 params[:disposition_suggested],
-                                                                assignee_id)
+                                                                user_validation.first.id)
                       uniques[file_rep.id] = sha256
                     else
                       duplicates << sha256
