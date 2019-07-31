3300db1f25130022f76354b48738260f6923b63d
nherbert <nherbert@sourcefire.com>
2019-07-31 16:56:47 -0400

    using rails.logger.error instead of puts

--- app/models/complaint_entry_screenshot.rb
+++ app/models/complaint_entry_screenshot.rb
@@ -11,7 +11,7 @@ class ComplaintEntryScreenshot < ApplicationRecord
       #go to the url provided (it needs http or https on it.)
       host_lookup = self.complaint_entry.hostlookup
       url = host_lookup.match(/^(http|https):\/\//) ? host_lookup : "http://#{host_lookup}"
-      puts ("Screenshotting url:#{url}")
+      Rails.logger.error ("Screenshotting url:#{url}")
       driver.navigate.to url
 
       #set the size to something respectable
@@ -19,17 +19,17 @@ class ComplaintEntryScreenshot < ApplicationRecord
 
       #save the screenshot hopefully in the database so we dont have to worry about disk usage
       self.update(screenshot: driver.screenshot_as(:png), error_message: "")
-      puts ("done with #{url}")
+      Rails.logger.error ("done with #{url}")
     rescue Net::ReadTimeout => ex
-      puts ("Hey! There was a Net Read Timeout error")
+      Rails.logger.error ("Hey! There was a Net Read Timeout error")
       file_data = File.open("app/assets/images/timeout_screenshot.jpg").read()
       self.update(screenshot: file_data, error_message: ex.message)
     rescue Exception => ex
       unless driver.nil?
         driver.close()
       end
-      puts ("oops there was a screen capture error")
-      puts ("#{ex.class}: #{ex.message}")
+      Rails.logger.error ("oops there was a screen capture error")
+      Rails.logger.error ("#{ex.class}: #{ex.message}")
       file_data = File.open("app/assets/images/failed_screenshot.jpg").read()
       self.update(screenshot: file_data, error_message: ex.message)
     ensure # this is a good practice to get into so that the driver will always exit, even if there is an error
