d3bde3aca509807bd103953cf2ad17bea155126f
micah <mimcfarl@cisco.com>
2019-08-07 09:12:40 -0400

    Remove certain WL rules from graph display

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -1765,7 +1765,19 @@ class Dispute < ApplicationRecord
       end
     end
 
-    rulehit_types.delete("dotq") # According to Jayme, "dotq" hit counts can be omitted (https://jira.vrt.sourcefire.com/browse/WEB-5082)
+    rulehit_types.reject! {|k, v| %w"dotq
+      alx_cln
+      tuse
+      a500
+      deli
+      csdw
+      suwl
+      ciwl
+      vsvd
+      wlh
+      wlm
+      wlw".include? k } # According to Jayme, hit counts for these rules can be omitted (https://jira.vrt.sourcefire.com/browse/WEB-5082)
+
     rulehit_types = Hash[rulehit_types.sort_by {|k,v| v}[0..24].reverse] # Limit rule hits to the top 25
 
     final_data = {}
