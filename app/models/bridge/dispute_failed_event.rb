c361f8c02644257a83e6267b3202064cbf441093
Nick Herbert <nherbert@avavpn-200.vrt.sourcefire.com>
2020-12-01 14:37:41 -0500

    adding logger files to DNM branch

--- app/models/bridge/dispute_failed_event.rb
+++ app/models/bridge/dispute_failed_event.rb
@@ -2,6 +2,7 @@ class Bridge::DisputeFailedEvent < Bridge::BaseMessage
   def initialize(addressee:, source_authority: nil, source_key: nil)
     super(channel: 'ticket-acknowledge',
           addressee: addressee)
+    Delayed::Worker.logger.info("Dispute failed event init")
     @source_authority = source_authority
     @source_key = source_key
   end
@@ -9,6 +10,7 @@ class Bridge::DisputeFailedEvent < Bridge::BaseMessage
   def post(source_key: @source_key, ac_status: Dispute::AC_FAILED)
     super(message: {source_key: source_key,
                     ac_status: ac_status})
+    Delayed::Worker.logger.info("Dispute failed event sending reply to bridge")
   end
   handle_asynchronously :post, :queue => "dispute_failed", :priority => 1
 end
--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -6,5 +6,19 @@ module Delayed
         write_attribute :last_error, err_message.truncate(1000000, omission: "...error message truncated")
       end
     end
+    module ActiveRecord
+      class Job
+        class << self
+          alias_method :reserve_original, :reserve
+          def reserve(worker, max_run_time = Worker.max_run_time)
+            previous_level = ::ActiveRecord::Base.logger.level
+            ::ActiveRecord::Base.logger.level = Logger::WARN if previous_level < Logger::WARN
+            value = reserve_original(worker, max_run_time)
+            ::ActiveRecord::Base.logger.level = previous_level
+            value
+          end
+        end
+      end
+    end
   end
 end
