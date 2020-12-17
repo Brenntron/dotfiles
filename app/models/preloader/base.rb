272dccb0c6444d955c2dc0d5fe75152f537d98c1
Chris LaClair <claclair@Chriss-MacBook-Pro.local>
2020-12-17 09:10:32 -0500

    WEB-6504: fixed possible bug in preloader base

--- app/models/preloader/base.rb
+++ app/models/preloader/base.rb
@@ -84,9 +84,9 @@ class Preloader::Base
         pretty_umbrella_status = "Unclassified" # Default or "0"
         case
           # Per docs here: https://dashboard.umbrella.com/o/1755319/#overview
-        when @umbrella[:status] == "-1"
+        when @umbrella[host]["status"] == "-1"
           pretty_umbrella_status = "Malicious"
-        when @umbrella[:status] == "1"
+        when @umbrella[host]["status"] == "1"
           pretty_umbrella_status = "Benign"
         end
         pretty_umbrella_status
