db101a8404a0a6609070e4a0e3dc445a6ad881ca
micah <mimcfarl@cisco.com>
2021-03-11 09:23:16 -0500

    Get the amp detection immediately after saving a dispute, rather than waiting for someone to view the show page for it

--- app/models/file_reputation_dispute.rb
+++ app/models/file_reputation_dispute.rb
@@ -10,6 +10,8 @@ class FileReputationDispute < ApplicationRecord
   has_many :file_rep_comments
   has_many :dispute_emails
 
+  after_save :update_amp_detection
+
   delegate :name, :email, :company, :company_name, :company_id, to: :customer, allow_nil: true, prefix: true
 
   STATUS_NEW                = 'NEW'
@@ -1154,5 +1156,42 @@ class FileReputationDispute < ApplicationRecord
       FileReputationApi::ReversingLabs.unsubscribe(candidate.sha256_hash)
     end
   end
+
+  private
+
+  def update_amp_detection
+      # Check whether AMP API is disabled
+      if Rails.configuration.amp_poke.host.blank?
+        raise "AMP Poke API is disabled or not configured"
+        # If we're on staging, test to see whether AMP API is enabled
+      elsif Rails.env == 'staging'
+        begin
+          FileReputationApi::Detection.get_bulk('1eba23049d725aabd84b63f8cd4b079c78f26cde6f7bb8be1d2477df0c0d1234')
+        rescue Exception
+          raise "AMP Poke API is currently disabled on staging"
+        end
+      end
+
+      detection = FileReputationApi::Detection.get_bulk(self.sha256_hash)
+      detection_last_set = FileReputationApi::ElasticSearch.query(self.sha256_hash)
+      last_fetched = Time.now.utc
+
+      begin
+        file_rep_disputes = FileReputationDispute.where(sha256_hash: detection.sha256_hash)
+        file_rep_dispute = file_rep_disputes.first
+        if detection_last_set == 'No history to display' && !file_rep_dispute.detection_last_set.nil?
+          detection_last_set = file_rep_dispute.detection_last_set
+        end
+        # TODO: I don't necessarily know that we need to .update_all on an after_save callback,
+        # but leaving it in here just to be safe. If this method causes performance problems,
+        # change this line first. Update only `self` instead of updating everything.
+        file_rep_disputes.update_all(disposition: detection.disposition,
+                                     detection_name: detection.name,
+                                     detection_last_set: detection_last_set,
+                                     last_fetched: last_fetched)
+      rescue
+        Rails.logger.error("Error saving updated detection information -- #{$!.message}")
+      end
+    end
 
 end
