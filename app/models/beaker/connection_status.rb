d2e6c362520b4ec0b08298d19985f7a6279b8154
Marlin W. Pierce <marlpier@cisco.com>
2021-04-16 10:53:40 -0400

    rename stub method.

--- app/models/beaker/connection_status.rb
+++ app/models/beaker/connection_status.rb
@@ -2,17 +2,17 @@ require 'service-connection-status_services_pb'
 
 class Beaker::ConnectionStatus < Beaker::BeakerBase
 
-  def self.stub
-    @stub = Talos::Service::ConnectionStatus::Stub.new(hostport, creds)
+  def self.remote_stub
+    @remote_stub = Talos::Service::ConnectionStatus::Stub.new(hostport, creds)
   end
 
-  def stub
-    self.class.stub
+  def remote_stub
+    self.class.remote_stub
   end
 
   # Request a Connection Status message.
   # rpc :ConnectionStatus, ::Talos::ConnectionStatus::Request, ::Talos::ConnectionStatus::Reply
   def self.connection_status
-    stub.connection_status(Talos::ConnectionStatus::Request.new)
+    remote_stub.connection_status(Talos::ConnectionStatus::Request.new)
   end
 end
