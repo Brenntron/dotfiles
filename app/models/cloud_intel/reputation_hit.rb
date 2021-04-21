83461a74650456485b79dd77e46a353dbeefd888
Marlin W. Pierce <marlpier@cisco.com>
2021-04-21 09:13:55 -0400

    Create Reputation Hit class.

---
+++ app/models/cloud_intel/reputation_hit.rb
@@ -0,0 +1,17 @@
+class CloudIntel::ReputationHit
+  # Gets mnemonics of multiple IP addresses
+  #
+  # example:
+  #   ReputiationHit.ip_mnemonics(["2.3.4.5", "35.236.52.109"])
+  #   => { "2.3.4.5" => {mnemonics: ['abc', 'def']},
+  #        "35.236.52.109" => {mnemonics: ['ghi']}}
+  #
+  # @param [Array<String>] ips the IP addresses, e.g. ["2.3.4.5", "35.236.52.109"]
+  # @return [Hash] keys are inputted IP addresses, values are hash with array of mnemonics
+  def self.ip_mnemonics(ips)
+    byebug
+    reply = Beaker::Ipd.query_reputation(ips)
+    byebug
+    reply
+  end
+end
