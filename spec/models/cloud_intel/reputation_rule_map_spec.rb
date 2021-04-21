c2a5244012a480728e8ec70927001c0eb3d69136
Marlin W. Pierce <marlpier@cisco.com>
2021-04-21 16:23:51 -0400

    Context for let.

--- spec/models/cloud_intel/reputation_rule_map_spec.rb
+++ spec/models/cloud_intel/reputation_rule_map_spec.rb
@@ -1,16 +1,18 @@
 require "rails_helper"
 
 RSpec.describe CloudIntel::ReputationRuleMap do
-  let (:reputation_rule_map) { FactoryBot.build(:grpc_reputation_rule_map) }
-  let (:reputation_rule_map_version) {reputation_rule_map.version}
-  let (:first_rep_rule_id) {reputation_rule_map.rules.first.rep_rule_id}
-  let (:first_rule_mnemonic) {reputation_rule_map.rules.first.rule_mnemonic}
+  context "Normal reputation rule map from network" do
+    let (:reputation_rule_map) { FactoryBot.build(:grpc_reputation_rule_map) }
+    let (:reputation_rule_map_version) {reputation_rule_map.version}
+    let (:first_rep_rule_id) {reputation_rule_map.rules.first.rep_rule_id}
+    let (:first_rule_mnemonic) {reputation_rule_map.rules.first.rule_mnemonic}
 
-  it "should return a mnemonic from rep_rule_id" do
-    allow_any_instance_of(Talos::Service::IPD::Stub).to receive(:query_rule_map).and_return(reputation_rule_map)
+    it "should return a mnemonic from rep_rule_id" do
+      allow_any_instance_of(Talos::Service::IPD::Stub).to receive(:query_rule_map).and_return(reputation_rule_map)
 
-    mnemonic = CloudIntel::ReputationRuleMap.mnemonic(first_rep_rule_id, version: reputation_rule_map_version)
+      mnemonic = CloudIntel::ReputationRuleMap.mnemonic(first_rep_rule_id, version: reputation_rule_map_version)
 
-    expect(mnemonic).to eql(first_rule_mnemonic)
+      expect(mnemonic).to eql(first_rule_mnemonic)
+    end
   end
 end
