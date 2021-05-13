4719ebea62e08457d70733cd47755df622d362b8
micah <mimcfarl@cisco.com>
2021-05-13 12:23:09 -0400

    yes you are reading correctly. literally this was the only change needed

--- app/controllers/api/v1/escalations/file_rep/amp_naming_convention.rb
+++ app/controllers/api/v1/escalations/file_rep/amp_naming_convention.rb
@@ -52,7 +52,7 @@ module API
                   timestamp = Time.now
                   Rails.logger.debug("\n\n*** PATCH #{params['patterns']}\n\n")
                   ::AmpNamingConvention.save_from_params(params['patterns'])
-                  # ::AmpNamingConvention.send_all_to_ti(timestamp: timestamp)
+                  ::AmpNamingConvention.send_all_to_ti(timestamp: timestamp)
                 end
 
                 render json: {status: 'Success'}
