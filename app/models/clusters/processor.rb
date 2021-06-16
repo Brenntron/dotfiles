10c9f933ed38bec0851dadebabb9a5021af71c96
vmurzak <vmurzak@cisco.com>
2021-06-17 00:16:17 +0300

    WEB-6982 Add 3rd person review handling

--- app/models/clusters/processor.rb
+++ app/models/clusters/processor.rb
@@ -29,8 +29,15 @@ class Clusters::Processor
 
   def process!
     clusters.each do |cluster|
-      PLATFORM_PROVIDERS[cluster[:platform]].new(cluster, user).process
-      # TODO: add credit to the user
+      if third_person_review_cluster?(cluster)
+        manager_user = User.where(cvs_username: Complaint::MAIN_WEBCAT_MANAGER_CONTACT).first
+        ClusterAssignment.assign_pemanent!(cluster[:domain], manager_user)
+
+        raise 'Cluster should pass manager review'
+      else
+        PLATFORM_PROVIDERS[cluster[:platform]].new(cluster, user).process
+        # TODO: add credit to the user
+      end
     end
   end
 
@@ -39,5 +46,18 @@ class Clusters::Processor
       PLATFORM_PROVIDERS[cluster[:platform]].new(cluster, user).decline
       # TODO: add credit to the user
     end
+  end
+
+  private
+
+  def third_person_review_cluster?
+    # third person review - should be done by webcat manager
+    return false if user.is_webcat_manager? # webcat managers skip 3rd person review
+
+    prefix = cluster[:domain]
+    categories = JSON.parse(cluster[:categories].map(&:to_i))
+
+    verdict_check = Webcat::EntryVerdictChecker.new(prefix, categories).check
+    !verdict_check[:verdict_pass]
   end
 end
