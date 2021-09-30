9b5b95a3290db949c85f86035083f1ba7c604b67
Marlin W. Pierce <marlpier@cisco.com>
2021-09-30 10:24:21 -0400

    Enable memcache.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,15 +16,13 @@ Rails.application.configure do
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
   end
+  config.cache_store = :mem_cache_store
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
