475286c041d5ce0e951ebe4254015fe78bb31d4e
jewillin <jewillin@cisco.com>
2022-04-14 11:27:07 -0500

    WEB-8896: Fix sdr dispute age helper to be consistent with webrep's age. Fix styling issues for sdr disputs show page.

--- app/helpers/escalations/sdr/disputes_helper.rb
+++ app/helpers/escalations/sdr/disputes_helper.rb
@@ -1,15 +1,17 @@
 module Escalations::Sdr::DisputesHelper
-  def dispute_age(created_at)
-    seconds_diff = (Time.zone.now - created_at).to_i.abs
-
-    hours = seconds_diff / 3600
-    seconds_diff -= hours * 3600
-
-    minutes = seconds_diff / 60
-    seconds_diff -= minutes * 60
-
-    seconds = seconds_diff
-
-    '%02dhr %02dmin' % [hours, minutes]
+  def dispute_age(assigned_at)
+    return '' unless assigned_at
+    age = assigned_at - DateTime.now
+    age = age.abs # lazy
+    mm, ss = age.divmod(60)
+    hh, mm = mm.divmod(60)
+    dd, hh = hh.divmod(24)
+    if dd > 0
+      "%dd %dh" % [dd, hh]
+    elsif hh > 0
+      "%dh %dm" % [hh, mm]
+    elsif hh == 0
+      "<1 hr"
+    end
   end
 end
