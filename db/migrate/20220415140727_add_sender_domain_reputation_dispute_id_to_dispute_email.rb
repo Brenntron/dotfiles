815ae1338b764ffe58b9e62b8429c126543ec48d
jewillin <jewillin@cisco.com>
2022-04-28 14:20:50 -0500

    WEB-8897: Remove duplicate migration.

--- db/migrate/20220415140727_add_sender_domain_reputation_dispute_id_to_dispute_email.rb
+++
@@ -1,5 +0,0 @@
-class AddSenderDomainReputationDisputeIdToDisputeEmail < ActiveRecord::Migration[5.2]
-  def change
-    add_column :dispute_emails, :sender_domain_reputation_dispute_id, :integer
-  end
-end
