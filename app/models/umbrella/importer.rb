b5520941dbe7d97d9a057e413f2d1958f015837d
Bohdan Prepelytsia <prepel99@gmail.com>
2022-05-20 17:59:30 +0300

    Added destroying not needed clusters

--- app/models/umbrella/importer.rb
+++ app/models/umbrella/importer.rb
@@ -5,12 +5,23 @@ class Umbrella::Importer
     end
 
     def import_without_delay(date = nil)
+      destroy_existing_clusters!
       import_new_clusters(date)
     end
 
     private
 
+      def destroy_existing_clusters!
+        # destroys Umbrella clusters that should be replaced by new import
+        # it also describes specific logic if we need to leave some specific clusters
+        # destroys everything except assigned or pending clusters
+
+        assigned_domains = ClusterAssignment.fetch_all_assignments.pluck(:domain)
+        UmbrellaCluster.where.not(status: :pending, domain: assigned_domains).delete_all
+      end
+
       def handle_import(date)
+        destroy_existing_clusters!
         import_new_clusters(date)
       end
       handle_asynchronously :handle_import, run_at: Proc.new { Time.zone.today.beginning_of_day + 20.hours } # run at 8pm UTC today
