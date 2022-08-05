53d95bc4adf42b15997446e4faf53bf0506a2685
mkaban <mkaban@cisco.com>
2022-08-05 19:01:41 +0300

    WEB-9649 map Beaker SDR information to human-readable values

--- app/helpers/escalations/sdr/disputes_helper.rb
+++ app/helpers/escalations/sdr/disputes_helper.rb
@@ -20,4 +20,20 @@ module Escalations::Sdr::DisputesHelper
     extracted_details = description.split(".\r")[0]
     extracted_details = extracted_details.split(',')
   end
+
+  def humanize_beaker_info(beaker_info)
+    beaker_data = JSON.parse(beaker_info).with_indifferent_access
+    entry = beaker_data.dig('response', 'data').keys.first
+    data = beaker_data.dig('response', 'data', entry)
+
+    new_data = data.except('threat_level_id', 'query_ts', 'threat_cat_id').merge(
+      {
+        threat_level: CloudIntel::ThreatLevelMap.get_threat_level_mnemonic(data['threat_level_id']),
+        threat_category: CloudIntel::ThreatCatMap.threat_category_by_id(data.dig('threat_cat_id').first),
+        query_time: Time.at(data['query_ts'] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+      }
+    )
+    beaker_data['response']['data'][entry] = new_data
+    beaker_data.to_json
+  end
 end
