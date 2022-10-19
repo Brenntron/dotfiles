3a70a785763d47312fac6937c1130a2215ac7314
rfennell <rfennell@cisco.com>
2022-10-19 15:04:02 -0400

    adding enrichment health check to api_status page

--- app/models/enrichment_service/query_interface.rb
+++ app/models/enrichment_service/query_interface.rb
@@ -1,4 +1,7 @@
 class EnrichmentService::QueryInterface
+
+  TEST_URL = "www.google.com"
+
   def self.interpreted_query(query_item)
     suggestion = get_suggestion(query_item)
     case suggestion
@@ -84,6 +87,45 @@ class EnrichmentService::QueryInterface
     end
     params['mapped_taxonomy'] = EnrichmentService::TaxonomyMap.get_taxonomy(context_tag['taxonomy_id'], context_tag['taxonomy_entry_id'])
     params
+  end
+
+  def self.health_check
+    health_report = {}
+
+    times_to_try = 3
+    times_tried = 0
+    times_successful = 0
+    times_failed = 0
+    is_healthy = false
+
+    (1..times_to_try).each do |i|
+      begin
+
+        result = interpreted_query(TEST_URL)
+
+        if result["context_tags"].present?
+          times_successful += 1
+        else
+          times_failed += 1
+        end
+        times_tried += 1
+      rescue
+        times_failed += 1
+        times_tried += 1
+      end
+
+    end
+
+    if times_successful > times_failed
+      is_healthy = true
+    end
+
+    health_report[:times_tried] = times_tried
+    health_report[:times_successful] = times_successful
+    health_report[:times_failed] = times_failed
+    health_report[:is_healthy] = is_healthy
+
+    health_report
   end
 
 end
