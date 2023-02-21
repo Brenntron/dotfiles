41db312f63c1ee63964f74c159e806571569654f
Chris LaClair <claclair@avavpn-203.vrt.sourcefire.com>
2023-02-21 13:33:04 -0500

    WEB-10454: backing these deletions out since may need for the file migration to local first

--- app/models/bugzilla_rest/base_error.rb
+++ app/models/bugzilla_rest/base_error.rb
@@ -1,8 +1,13 @@
 # Exception class specific to bugzilla REST API
+# marked for removal but may need for data migration from bugzilla first
 class BugzillaRest::BaseError < StandardError
-  attr_reader :code
+attr_reader :code
 
   alias_method :status, :code
 
+  def initialize(msg = 'Bugzilla REST API Error', code:)
+    super(msg)
+    @code = code
+  end
 
 end
