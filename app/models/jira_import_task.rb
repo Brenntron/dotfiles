bd58bcd228898ca81e997a57dd98aedda57189e0
Timothy Porter <tporter@timothys-mbp.vrt.sourcefire.com>
2023-03-29 13:02:32 -0400

    import task changes

--- app/models/jira_import_task.rb
+++ app/models/jira_import_task.rb
@@ -12,6 +12,8 @@ class JiraImportTask < ApplicationRecord
 
   #Read CSV from Jira and send URLs to Bast
   def process_import
+    update(imported_at: Time.now)
+
     issue = JiraRest::Issue.new(issue_key)
     attachments = issue.attachments_data
     if attachments.empty?
@@ -21,9 +23,9 @@ class JiraImportTask < ApplicationRecord
     attachment_to_process = attachments.first
     if attachment_to_process[:type] == VALID_FILE_TYPE
       raw_data = attachment_to_process[:content]
-      filtered_data = raw_data.map {|m| m.gsub("\"", '').strip}.reject {|r| r.blank?}
+      filtered_data = raw_data.map {|m| m.gsub("\"", '').gsub(',', '').strip}.reject {|r| r.blank?}
       filtered_data.each do |url|
-        import_urls.create(submitted_url: url)
+        import_urls.find_or_create_by(submitted_url: url)
       end
 
       begin
