21e7b750c85bc0b6a0e0219c2a65d33b62123b8e
Timothy Porter <tporter@avavpn-178.vrt.sourcefire.com>
2023-04-12 13:38:07 -0400

    add scopes for counts

--- app/models/jira_import_task.rb
+++ app/models/jira_import_task.rb
@@ -3,6 +3,11 @@ class JiraImportTask < ApplicationRecord
 
   validates :issue_key, presence: true, uniqueness: true
 
+  scope :total_count, -> { all.count }
+  scope :completed_count, -> { where(status: STATUS_COMPLETE).count }
+  scope :failed_count, -> { where(status: STATUS_FAILURE).count }
+  scope :pending_count, -> { where(status: STATUS_PENDING).count }
+
   STATUS_COMPLETE = "Complete"
   STATUS_FAILURE = "Failure"
   STATUS_PENDING = "Pending"
