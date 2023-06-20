4ffae8524ba338aebed196ac8fa038b1edad7ec8
Timothy Porter <tporter@scavpn-060.vrt.sourcefire.com>
2023-06-20 14:57:03 -0400

    add health checks for jira and bast

--- app/models/bast/base.rb
+++ app/models/bast/base.rb
@@ -35,4 +35,41 @@ class Bast::Base
   def self.get_task_result(task_id)
     call_request_parsed(:get, "api/download_json/#{task_id}")
   end
+
+  def self.health_check
+    health_report = {}
+
+    times_to_try = 3
+    times_tried = 0
+    times_successful = 0
+    times_failed = 0
+    is_healthy = false
+
+    (1..times_to_try).each do |i|
+      begin
+        result = get_task_status(300)
+        if result["status"].present?
+          times_successful += 1
+        else
+          times_failed += 1
+        end
+        times_tried += 1
+      rescue
+        times_failed += 1
+        times_tried += 1
+      end
+
+    end
+
+    if times_successful > times_failed
+      is_healthy = true
+    end
+
+    health_report[:times_tried] = times_tried
+    health_report[:times_successful] = times_successful
+    health_report[:times_failed] = times_failed
+    health_report[:is_healthy] = is_healthy
+
+    health_report
+  end
 end
