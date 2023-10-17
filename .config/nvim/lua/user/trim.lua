27bdefd37c118a7451dbd9a1483c1fee91f9d906
jewillin <jewillin@cisco.com>
2023-10-17 09:23:03 -0500

    Fix trouble, fzy_native and hopefully dadbod plugins.

--- .config/nvim/lua/user/trim.lua
+++ .config/nvim/lua/user/trim.lua
@@ -1,9 +1,10 @@
 local M = {
   "cappyzawa/trim.nvim",
   event = "BufRead",
-  config = function()
-    require("trim").setup { trim_last_line = false }
-  end,
 }
+
+function M.config()
+  require("trim").setup { trim_last_line = false }
+end
 
 return M
--- .config/nvim/lua/user/vim-dadbod-ui.lua
+++ .config/nvim/lua/user/vim-dadbod-ui.lua
@@ -1,3 +1,18 @@
-local M = { "kristijanhusak/vim-dadbod-ui", dependencis = "tpope/vim-dadbod" }
+local M = {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    vim.g.db_ui_user_nerd_fonts = 1
+  end,
+}
 
 return M
