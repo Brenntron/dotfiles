0b3ccd7f3da76c051539d9f4230077680f66d3a3
Brennan Willingham <brennan.willingham@gmail.com>
2024-01-22 15:48:17 -0600

    Commit for update from mac.

--- .config/linux/linux.zsh
+++ .config/linux/linux.zsh
@@ -1,5 +1,14 @@
 export ZPLUG_HOME=$HOME/.zplug
 
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 if [ -x "$(command -v batcat)" ]; then
   alias cat="batcat"
 fi
+
+export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --enable-sctp --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit --with-ssl=/usr/local/Cellar/openssl/1.0.2p"
