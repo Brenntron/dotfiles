feb3d163c5c82b7a9603293bc8ef245d7f45b03c
Brennan Willingham <brennan.willingham@gmail.com>
2024-02-14 08:26:00 -0600

    Update copilot setup for neovim to interface with cmp.

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,20 @@
 local M = {
-  "github/copilot.vim",
-  event = "VimEnter",
+  "zbirenbaum/copilot.lua",
+  event = {"LspAttach", "VimEnter" },
 }
 
-function M.config() end
+function M.config()
+  require('copilot').setup {
+    copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/shims/node",
+    filetypes = {
+      sh = function ()
+        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0))_, '^%.env.*') then
+          return false
+        end
+        return true
+      end,
+    },
+  }
+end
 
 return M
