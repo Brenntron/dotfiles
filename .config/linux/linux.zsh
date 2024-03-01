10990ca118fc8890290b3d9ebfc1ab2dbaad8d5c
Brennan Willingham <brennan.willingham@gmail.com>
2024-03-01 12:18:27 -0600

    Replace docker with podman.

--- .config/linux/linux.zsh
+++ .config/linux/linux.zsh
@@ -1,5 +1,4 @@
 alias cat="batcat"
-alias drun='docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $(pwd):/pwd'
 
 export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --enable-sctp --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit --with-ssl=/usr/local/Cellar/openssl/1.0.2p"
 export SSH_AUTH_SOCK=~/.1password/agent.sock
@@ -21,12 +20,11 @@ plugins=(
   copyfile
   copypath
   cp
-  docker
-  docker-compose
   fzf
   gh
   git
   gpg-agent
+  podman
   rails
   ruby
   ssh-agent
