34a561a1e2a2fcd6874fdd4d3196ca5a64e772b4
Brennan Willingham <brennan.willingham@gmail.com>
2024-03-01 17:20:02 -0600

    Update plugins and dependency management.

--- .config/linux/linux.zsh
+++ .config/linux/linux.zsh
@@ -1,9 +1,11 @@
 export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --enable-sctp --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit --with-ssl=/usr/local/Cellar/openssl/1.0.2p"
 export SSH_AUTH_SOCK=~/.1password/agent.sock
 
-require "autoenv" "npm install -g '@hyerupcall/autoenv'"
 require "batcat" "sudo apt update;sudo apt install bat -y"
+require "fnt" "sudo apt update;sudo apt install fnt -y"
+require "find" "sudo apt update;sudo apt install findutils"
 require "fzf" "sudo apt update;sudo apt install fzf -y"
 require "gcc" "sudo apt update;sudo apt install build-essential -y"
+require "oh-my-posh" "sudo apt update;sudo apt install oh-my-posh -y"
 
 alias cat="batcat"
--- .config/osx/osx.zsh
+++ .config/osx/osx.zsh
@@ -4,15 +4,16 @@ ssh-add --apple-use-keychain
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/opt/imagemagick@6/bin:$PATH"
 export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
-export PATH="$PATH::$(yarn global bin)"
 export PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
 export KERL_CONFIGURE_OPTIONS="--disabled-debug --without-javac --disable-hipe --with-ssl=$(brew --prefix openssl)"
 export NODE_OPTIONS="--max-old-space-size=8192"
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
-require "autoenv" "brew install autoenv"
 require "bat" "brew install bat"
-requrie "fzf" "brew install fzf"
+require "fnt" "brew install fnt"
+require "find" "brew install findutils"
+require "fzf" "brew install fzf"
 require "gcc" "brew install gcc"
+require "oh-my-posh" "brew install oh-my-posh"
 
 alias cat="bat"
