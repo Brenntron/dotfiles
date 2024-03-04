2be02d28601785481175f52d42a08883124d3bb3
Brennan Willingham <brennan.willingham@gmail.com>
2024-03-04 17:11:06 -0600

    Update fzf base.

--- .config/linux/linux.zsh
+++ .config/linux/linux.zsh
@@ -3,6 +3,4 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --enable-sctp --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit --with-ssl=/usr/local/Cellar/openssl/1.0.2p"
 export SSH_AUTH_SOCK=~/.1password/agent.sock
 
-require "batcat" "sudo apt update;sudo apt install bat -y"
-
 alias cat="batcat"
