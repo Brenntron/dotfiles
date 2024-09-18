86c8b1e58dbb9453327508b2897c51f2494da00c
Brennan Willingham <brennan.willingham@gmail.com>
2024-09-18 10:50:04 -0500

    Change to catppuccin theme, add navbuddy and harpoon to neovim.

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -5,9 +5,13 @@ local M = {
     {
       "nvim-tree/nvim-web-devicons",
     },
+    {
+      "catppuccin/nvim",
+    },
   },
 }
 function M.config()
+  local macchiato = require("catppuccin.palettes").get_palette "macchiato"
   require("bufferline").setup {
     options = {
       close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
@@ -16,11 +20,14 @@ function M.config()
       separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
       show_tab_indicators = true, -- | "always" | "never",
     },
-    highlights = {
-      buffer_selected = {
-        bold = true,
-        italic = false,
-      },
+    -- tokyo night highlights
+    -- highlights = {
+    --   buffer_selected = {
+    --     bold = true,
+    --     italic = false,
+    --   },
+    highlights = require("catppuccin.groups.integrations.bufferline").get {
+      styles = { "italic", "bold" },
     },
   }
 end
--- .fzf.zsh
+++ .fzf.zsh
@@ -2,7 +2,15 @@
 # ---------
 export FZF_BASE="$(brew --prefix fzf)"
 export FZF_DEFAULT_COMMAND='fd -H -i'
-export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --preview "cat --color=always {}" --preview-window "~3"'
+# Tokyo Night
+# export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --preview "cat --color=always {}" --preview-window "~3"'
+# Catpuccin Macchiato
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+--color=selected-bg:#494d64 \
+--multi"
 export FZF_PATH="${FZF_BASE}/bin"
 
 if [[ ! "$PATH" == *$FZF_PATH* ]]; then
