1b7f80bd85f88dfd62ef388dae283fc0f52dcb12
Brennan Willingham <brennan.willingham@gmail.com>
2024-09-18 06:56:09 -0500

    Update asdf settings, change global ruby version and update toggleterm config.

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -71,7 +71,7 @@ function M.config()
   require("toggleterm").setup {
     size = function(term)
       if term.direction == "horizontal" then
-        return 60
+        return 20
       elseif term.direction == "vertical" then
         return vim.o.columns * 0.4
       end
@@ -83,7 +83,7 @@ function M.config()
     shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
     start_in_insert = true,
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
-    persist_size = false,
+    persist_size = true,
     direction = "float",
     close_on_exit = true, -- close the terminal window when the process exits
     shell = '/bin/zsh', -- change the default shell
