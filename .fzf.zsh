2f342616e7f5205e051def0a373ef54ddf347f65
Brennan Willingham <brennan.willingham@gmail.com>
2024-11-22 10:44:15 -0600

    Update fzf settings.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,8 @@
 # fzf config
 # ---------
 export FZF_BASE="$(brew --prefix fzf)"
-export FZF_DEFAULT_COMMAND='fd -H -i'
+export FZF_DEFAULT_COMMAND='fd --type file -H -i'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # Tokyo Night
 # export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --preview "cat --color=always {}" --preview-window "~3"'
 # Catpuccin Macchiato
