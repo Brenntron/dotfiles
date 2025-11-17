4063875e51febc40bbd09f2630190f1f997c61a3
Brennan Willingham <brennan.willingham@gmail.com>
2025-11-17 13:42:10 -0600

    Update apt packages for podman usage, attempt to fix copy and pasting in toggleterm.

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -21,3 +21,15 @@ vim.filetype.add({
     avanterules = 'markdown'
   }
 })
+
+-- kitty specific settings
+vim.opt.clipboard = ""
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+  },
+}
