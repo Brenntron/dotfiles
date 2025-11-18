12db2d8b8419f350f5367945738a72f942053469
Brennan Willingham <brennan.willingham@gmail.com>
2025-11-18 12:47:30 -0600

    Remove clipboard change as it was likely the cause of a yanky error.

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -24,12 +24,3 @@ vim.filetype.add({
 
 -- kitty specific settings
 vim.opt.clipboard = ""
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-  },
-}
