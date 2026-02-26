32822e7f211e1473d0ffab9b54a3f16da62ea4df
Brennan Willingham <brennan.willingham@gmail.com>
2026-02-26 10:41:41 -0600

    Alias rm to rip, update asdf to set JAVA_HOME, update packages.

--- dot_config/asdf/asdf_setup.zsh
+++ dot_config/asdf/asdf_setup.zsh
@@ -13,3 +13,6 @@ compdef _asdf asdf
 
 export ASDF_CONFIG_FILE="${HOME}/.config/asdf/.asdfrc"
 . ${ASDF_DATA_DIR:-$HOME/.asdf}/plugins/golang/set-env.zsh
+
+# Set JAVA_HOME
+. ~/.asdf/plugins/java/set-java-home.zsh
